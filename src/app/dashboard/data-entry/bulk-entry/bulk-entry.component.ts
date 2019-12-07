b6e40861f1dfa2fe909d2ce893c2fda3d7320aee
Shadab Faiz <shadab.faiz@dhwaniris.com>
2019-12-07 17:17:02 +0530

    :lipstcik: :boom: :sparkles: Implemented file upload progress

--- src/app/dashboard/data-entry/bulk-entry/bulk-entry.component.ts
+++ src/app/dashboard/data-entry/bulk-entry/bulk-entry.component.ts
@@ -1,53 +1,104 @@
-import { DataEntryService } from './../data-entry.service';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { map } from 'rxjs/operators';
+
+import { DataEntryService } from './../data-entry.service';
 
 @Component({
-    selector: 'app-bulk-entry',
-    templateUrl: './bulk-entry.component.html',
-    styleUrls: ['./bulk-entry.component.scss']
+  selector: "app-bulk-entry",
+  templateUrl: "./bulk-entry.component.html",
+  styleUrls: ["./bulk-entry.component.scss"]
 })
 export class BulkEntryComponent implements OnInit {
+  constructor(
+    private formBuilder: FormBuilder,
+    private dataEntryService: DataEntryService
+  ) {}
+  submitted = false;
+  years: any = [];
+  bulkEntryForm: FormGroup;
+  filesToUpload: Array<File> = [];
+  uploadResult: any;
 
-    submitted: boolean = false;
-    years: any = [];
-    bulkEntryForm: FormGroup;
-    filesToUpload: Array<File> = [];
-    uploadResult: any;
-    
-    constructor(private formBuilder: FormBuilder, private dataEntryService: DataEntryService) { }
+  fileUploadTracker: { [fileName: string]: number } = {};
+  fileProcessingTracker: {
+    [fileName: string]: "in-process" | "completed";
+  } = {};
 
-    ngOnInit() {
-        this.years = ['2015-16', '2016-17', '2017-18'];
-        this.bulkEntryForm = this.formBuilder.group({
-            year: [this.years[0], Validators.required],	
-        });
+  ngOnInit() {
+    this.years = ["2015-16", "2016-17", "2017-18"];
+    this.bulkEntryForm = this.formBuilder.group({
+      year: [this.years[0], Validators.required]
+    });
+  }
+
+  upload() {
+    this.submitted = true;
+    if (this.bulkEntryForm.invalid || !this.bulkEntryForm.get("year").value) {
+      return false;
     }
+    const formData: FormData = new FormData();
+    const files: Array<File> = this.filesToUpload;
+    formData.append("year", this.bulkEntryForm.get("year").value);
+    for (let i = 0; i < files.length; i++) {
+      this.uploadFile(files[i]);
+    }
+    // this.dataEntryService
+    //   .bulkEntry(formData)
+    //   .pipe(
+    //     map((response: HttpEvent<any>) => {
+    //       return this.logUploadProgess(response);
+    //     })
+    //   )
+    //   .subscribe(res => {
+    //     if (res["success"]) {
+    //       this.uploadResult = res["data"];
+    //       alert("Upload summary is available below");
+    //     }
+    //   });
+  }
 
-    upload() {
-        this.submitted = true;
-        if(this.bulkEntryForm.invalid || !this.bulkEntryForm.get('year').value){
-            return false;
+  uploadFile(file: File) {
+    const formData: FormData = new FormData();
+    formData.append("year", this.bulkEntryForm.get("year").value);
+    formData.append("files", file, file.name);
+    console.log("uploading ", file.name);
+    this.fileUploadTracker[file.name] = 0;
+    this.dataEntryService
+      .bulkEntry(formData)
+      .pipe(
+        map((response: HttpEvent<any>) => {
+          return this.logUploadProgess(response, file);
+        })
+      )
+      .subscribe(res => {
+        if (res["success"]) {
+          this.uploadResult = res["data"];
+          alert("Upload summary is available below");
         }
-        const formData: any = new FormData();
-        const files: Array<File> = this.filesToUpload;
-        console.log(files);
-        formData.append('year', this.bulkEntryForm.get('year').value);
-        for (let i = 0; i < files.length; i++) {
-            formData.append("files", files[i], files[i]['name']);
-        }
-        console.log('form data variable :   ' + formData.toString());
-        this.dataEntryService.bulkEntry(formData).subscribe(res =>{
-            if(res['success']){
-                this.uploadResult = res['data'];
-                alert('Upload summary is available below');
-            }
-        });
-    }
+      });
+  }
 
-    fileChangeEvent(fileInput: any) {
-        this.filesToUpload = <Array<File>>fileInput.target.files;
-        //this.product.photo = fileInput.target.files[0]['name'];
+  private logUploadProgess(event: HttpEvent<any>, file: File) {
+    if (event.type === HttpEventType.UploadProgress) {
+      console.log(event);
+      const percentDone = Math.round((100 * event.loaded) / event.total);
+      if (percentDone === 100) {
+        this.startFileProcessTracking(file);
+      }
+      this.fileUploadTracker[file.name] = percentDone;
     }
+    return event;
+  }
 
+  private startFileProcessTracking(file: File) {
+    this.fileProcessingTracker[file.name] = "in-process";
+  }
+
+  fileChangeEvent(fileInput: Event) {
+    this.filesToUpload = <Array<File>>fileInput.target["files"];
+    console.log(this.filesToUpload);
+    // this.product.photo = fileInput.target.files[0]['name'];
+  }
 }
--- src/app/dashboard/data-entry/data-entry.service.ts
+++ src/app/dashboard/data-entry/data-entry.service.ts
@@ -1,33 +1,48 @@
-import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { environment } from '../../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class DataEntryService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  bulkEntry(files){
-    const httpOptions = { headers: new HttpHeaders({'Accept': 'multipart/form-data'}) };
-    return this.http.post(environment.api.url + 'ledger/bulkEntry', files, httpOptions );
-
+  bulkEntry(files) {
+    const httpOptions = {
+      headers: new HttpHeaders({ Accept: "multipart/form-data" })
+    };
+    return this.http.post(environment.api.url + "ledger/bulkEntry", files, {
+      ...httpOptions,
+      reportProgress: true,
+      observe: "events"
+    });
   }
 
-  createEntry(ledgerForm){
-    const httpOptions = { headers: new HttpHeaders({'Accept': 'multipart/form-data'}) };
-    return this.http.post(environment.api.url + 'ledger/entry', ledgerForm, httpOptions );
+  createEntry(ledgerForm) {
+    const httpOptions = {
+      headers: new HttpHeaders({ Accept: "multipart/form-data" })
+    };
+    return this.http.post(
+      environment.api.url + "ledger/entry",
+      ledgerForm,
+      httpOptions
+    );
   }
 
-  getLedgerLogs(criteria){
-    return this.http.post(environment.api.url + 'ledger/getAll', criteria );
+  getLedgerLogs(criteria) {
+    return this.http.post(environment.api.url + "ledger/getAll", criteria);
   }
 
-
-  excelToJsonConvertor(files){
-    const httpOptions = { headers: new HttpHeaders({'Accept': 'multipart/form-data'}) };
-    return this.http.post(environment.api.url + 'ledger/excelToJsonConvertor', files, httpOptions );
+  excelToJsonConvertor(files) {
+    const httpOptions = {
+      headers: new HttpHeaders({ Accept: "multipart/form-data" })
+    };
+    return this.http.post(
+      environment.api.url + "ledger/excelToJsonConvertor",
+      files,
+      httpOptions
+    );
   }
-
 }
