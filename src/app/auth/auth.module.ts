44a3060f4c3fd3f2af5325dfbf92a91a70f8f0bc
Shadab Faiz <shadab.faiz@dhwaniris.com>
2019-12-12 18:58:05 +0530

    :lipstick: :sparkles: Shifted the table to a sperate module and routing

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,16 +3,15 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { CfChartsModule } from '../shared/cf-charts/cf-charts.module';
-import { PreLoaderComponent } from '../shared/components/pre-loader/pre-loader.component';
 import { AuthRouter } from './auth.router';
 import { AuthService } from './auth.service';
 import { HomeHeaderComponent } from './home-header/home-header.component';
-import { GridComponent } from './home/grid/grid.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { StatisticsComponent } from './statistics/statistics.component';
 
+// import { GridComponent } from './home/grid/grid.component';
 @NgModule({
   imports: [CommonModule, AuthRouter, ReactiveFormsModule, CfChartsModule],
   providers: [AuthService],
@@ -22,9 +21,8 @@ import { StatisticsComponent } from './statistics/statistics.component';
     RegisterComponent,
     HomeComponent,
     HomeHeaderComponent,
-    StatisticsComponent,
-    GridComponent,
-    PreLoaderComponent
+    StatisticsComponent
+    // GridComponent,
   ],
   exports: [HomeHeaderComponent]
 })
