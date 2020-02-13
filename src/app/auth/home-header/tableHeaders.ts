9b4bd14e38b0dd416e669b0536d30375688776e1
Anirudh Chauhan <anirudh.chauhan@dhwaniris.com>
2020-02-13 09:09:39 +0000

    Merged in feature/dashboard (pull request #150)
    Feature/dashboard
    
    * fixed in first tab
    
    * fixed css bugs
    reduces dialog width
    added close button
    
    * Merge branch 'development' of https://bitbucket.org/dhwanidevs/perfect-ui into feature/dashboard
    
    # Conflicts:
    #	src/app/auth/home/home-tab-view/home-tab-view.component.html
    #	src/app/auth/home/home-tab-view/home-tab-view.component.scss

---
+++ src/app/auth/home-header/tableHeaders.ts
@@ -0,0 +1,57 @@
+export const tableHeaders = [
+    [
+      {title: 'Population Category', click: true, id: 'populationCategory'},
+      {title: 'Number of ULBs', id: 'numOfUlb'},
+      {title: 'Own Revenue (A)', id: 'ownRevenue', description: '(Rs in crores)'},
+      {title: 'Revenue Expenditure (B)', id: 'revenueExpenditure', description: '(Rs in crores)'},
+      {title: 'Own Revenue %', id: 'ownRevenuePercentage', description: '(A/B)'},
+      {title: 'Min. Own Revenue %', id: 'minOwnRevenuePercentage'},
+      {title: 'Max. Own Revenue %', id: 'maxOwnRevenuePercentage'}
+    ],
+    [
+      {title: 'Population Category', id: 'populationCategory'},
+      {title: 'Number of ULBs', id: 'numOfUlb'},
+      {title: 'Tax Revenue (a)', id: 'taxRevenue'},
+      {title: 'Rental Income (b)', id: 'rentalIncome'},
+      {title: 'Fees & user charges (c)', id: 'feesAndUserCharges'},
+      {title: 'Own revenues (a+b+c)', id: 'ownRevenues'},
+      {title: 'Sale & hire charges', id: 'saleAndHireCharges'},
+      {title: 'Assigned revenue', id: 'assignedRevenue'},
+      {title: 'Grants', id: 'grants'},
+      {title: 'Interest Income', id: 'interestIncome'},
+      {title: 'Other Income', id: 'otherIncome'}
+    ],
+    [
+      {title: 'assignedRevenueAndCompensationCoverPercentage', id: 'assignedRevenueAndCompensationCoverPercentage'},
+      {title: 'coveredPercentage', id: 'coveredPercentage'},
+      {title: 'deficitFinanceByCapitalGrantsCoverPercentage', id: 'deficitFinanceByCapitalGrantsCoverPercentage'},
+      {title: 'interestIncomeCoverPercentage', id: 'interestIncomeCoverPercentage'},
+      {title: 'otherIncomeCoverPercentage', id: 'otherIncomeCoverPercentage'},
+      {title: 'ownRevenueCoverPercentage', id: 'ownRevenueCoverPercentage', description: '(A/B)'},
+      {title: 'revenueGrantsContributionAndSubsidiesCoverPercentage', id: 'revenueGrantsContributionAndSubsidiesCoverPercentage'},
+      {title: 'saleAndHireChargesCoverPercentage', id: 'saleAndHireChargesCoverPercentage'}
+    ],
+    [
+      {title: 'Population Category', click: true, id: 'populationCategory'},
+      {title: 'Establishment expense', id: 'establishmentExpense'},
+      {title: 'Administrative Expense', id: 'administrativeExpense'},
+      {title: 'Operational & Maint. Expense', id: 'operationalAndMaintananceExpense'},
+      {title: 'Interest & Finance Expense ', id: 'interestAndFinanceExpense'},
+      {title: 'Others', id: 'other'}
+    ],
+    [
+      {title: 'Population Category', id: 'populationCategory'},
+      {title: 'Number of ULBs', id: 'numOfUlb'},
+      {title: 'Cash & Bank Balance (Rs in crore)', id: 'cashAndBankBalance'}
+    ],
+    [
+      {title: 'Population Category', id: 'populationCategory'},
+      {title: 'Number of ULBs', id: 'numOfUlb'},
+      {title: 'Loans from Central Government', id: 'LoanFromCentralGovernment'},
+      {title: 'Loans from Financial Institutions including Banks', id: 'loanFromFIIB'},
+      {title: 'Loans from State Government', id: 'loanFromStateGovernment'},
+      {title: 'Total Debt', id: 'total'}
+    ]
+
+  ]
+;
