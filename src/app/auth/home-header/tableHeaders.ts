950956ccde8a858d6e359f93972f49aa8ee29cc5
anirudh <anirudh.chauhan@dhwaniris.com>
2020-02-19 19:24:23 +0530

    Tab with title Sources of Financing Revenue Expenditure working

--- src/app/auth/home-header/tableHeaders.ts
+++ src/app/auth/home-header/tableHeaders.ts
@@ -24,14 +24,13 @@ export const tableHeaders = [
   ],
   [
     {title: 'Population Category', id: 'populationCategory'},
-    {title: 'Assigned Revenue And Compensation Cover Percentage', id: 'assignedRevenueAndCompensationCoverPercentage'},
-    {title: 'Covered Percentage', id: 'coveredPercentage'},
-    {title: 'Deficit Finance By Capital Grants Cover Percentage', id: 'deficitFinanceByCapitalGrantsCoverPercentage'},
-    {title: 'Interest Income Cover Percentage', id: 'interestIncomeCoverPercentage'},
-    {title: 'Other Income Cover Percentage', id: 'otherIncomeCoverPercentage'},
-    {title: 'Own Revenue Cover Percentage', id: 'ownRevenueCoverPercentage', description: '(A/B)'},
-    {title: 'Revenue Grants Contribution And Subsidies Cover Percentage', id: 'revenueGrantsContributionAndSubsidiesCoverPercentage'},
-    {title: 'Sale And Hire Charges Cover Percentage', id: 'saleAndHireChargesCoverPercentage'}
+    {title: 'Assigned Revenue And Compensation', id: 'assignedRevenueAndCompensationCoverPercentage'},
+    {title: 'Deficit Finance By Capital Grants', id: 'deficitFinanceByCapitalGrantsCoverPercentage'},
+    {title: 'Interest Income', id: 'interestIncomeCoverPercentage'},
+    {title: 'Other Income ', id: 'otherIncomeCoverPercentage'},
+    {title: 'Own Revenue ', id: 'ownRevenueCoverPercentage', description: '(A/B)'},
+    {title: 'Revenue Grants Contribution And Subsidies', id: 'revenueGrantsContributionAndSubsidiesCoverPercentage'},
+    {title: 'Sale And Hire Charges ', id: 'saleAndHireChargesCoverPercentage'}
   ],
   [
     {title: 'Population Category', click: true, id: 'populationCategory'},
@@ -60,11 +59,37 @@ export const tableHeaders = [
     {title: 'Total Debt', id: 'total'}
   ]
 ];
-export const modalTableHeaders = [[
-  {title: 'ULB name', click: true, id: 'name'},
-  {title: 'Population', id: 'population'},
-  {title: 'Own Revenues (A) ', id: 'ownRevenue', description: '(Rs in crores)'},
-  {title: 'Revenue Expenditure (B)', id: 'revenueExpenditure', description: '(Rs in crores)'},
-  {title: 'Own Revenue % (A/B)', id: 'ownRevenuePercentage'},
-],];
+export const modalTableHeaders = [
+  [
+    {title: 'ULB name', click: true, id: 'name'},
+    {title: 'Population', id: 'population'},
+    {title: 'Own Revenues (A) ', id: 'ownRevenue', description: '(Rs in crores)'},
+    {title: 'Revenue Expenditure (B)', id: 'revenueExpenditure', description: '(Rs in crores)'},
+    {title: 'Own Revenue % (A/B)', id: 'ownRevenuePercentage'},
+  ],
+  [
+    {title: 'ULB name', id: 'name'},
+    {title: 'Population', id: 'population'},
+    {title: 'Tax Revenue (a)', id: 'taxRevenue'},
+    {title: 'Rental Income (b)', id: 'rentalIncome'},
+    {title: 'Fees & user charges (c)', id: 'feesAndUserCharges'},
+    {title: 'Own revenues', id: 'ownRevenues'},
+    {title: 'Sale & hire charges', id: 'saleAndHireCharges'},
+    {title: 'Assigned revenue', id: 'assignedRevenue'},
+    {title: 'Grants', id: 'grants'},
+    {title: 'Interest Income', id: 'interestIncome'},
+    {title: 'Other Income', id: 'otherIncome'}
+  ],
+  [
+    {title: 'ULB name', id: 'name'},
+    {title: 'Population', id: 'population'},
+    {title: 'Assigned Revenue And Compensation', id: 'assignedRevenueAndCompensationCoverPercentage'},
+    {title: 'Deficit Finance By Capital Grants', id: 'deficitFinanceByCapitalGrantsCoverPercentage'},
+    {title: 'Interest Income', id: 'interestIncomeCoverPercentage'},
+    {title: 'Other Income ', id: 'otherIncomeCoverPercentage'},
+    {title: 'Own Revenue ', id: 'ownRevenueCoverPercentage', description: '(A/B)'},
+    {title: 'Revenue Grants Contribution And Subsidies', id: 'revenueGrantsContributionAndSubsidiesCoverPercentage'},
+    {title: 'Sale And Hire Charges ', id: 'saleAndHireChargesCoverPercentage'}
+  ]
+];
 
