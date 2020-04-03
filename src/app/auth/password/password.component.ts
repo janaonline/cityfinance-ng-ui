7499c605f39d0ec10e08cd3d499629e46bf44c4c
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-04-03 13:54:40 +0530

    :sparkle: Integrated password request / reset apis

--- src/app/auth/password/password.component.ts
+++ src/app/auth/password/password.component.ts
@@ -1,3 +1,4 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -5,6 +6,7 @@ import { AuthService } from 'src/app/auth/auth.service';
 import { PasswordValidator } from 'src/app/util/passwordValidator';
 
 import { environment } from './../../../environments/environment';
+import { PasswordService } from './service/password.service';
 
 @Component({
   selector: "app-password",
@@ -24,18 +26,17 @@ export class PasswordComponent implements OnInit {
 
   public uiType: "request" | "reset";
   public errorMessage: string;
+  public successMessage: string;
+  private token: string;
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router,
-    private _activatedRoute: ActivatedRoute
+    private _activatedRoute: ActivatedRoute,
+    private _passwordService: PasswordService
   ) {
     this.initializeForms();
-    this._activatedRoute.params.subscribe(res => {
-      if (res.id === "request" || res.id === "reset") {
-        this.uiType = res.id;
-      }
-    });
+    this.validateUrl();
   }
 
   ngOnInit() {
@@ -43,11 +44,31 @@ export class PasswordComponent implements OnInit {
       this.badCredentials = res;
     });
   }
+
   get lf() {
     return this.passwordRequestForm.controls;
   }
 
-  submitPasswordResetRequest(form: FormGroup) {}
+  submitPasswordResetRequest(form: FormGroup) {
+    this.errorMessage = null;
+    console.log(`submitPasswordResetRequest `);
+    if (!form.valid) {
+      this.errorMessage = "Email ID is incorrect.";
+      return;
+    }
+
+    form.disable();
+
+    this._passwordService.requestPasswordReset(form.value).subscribe(
+      res => {
+        const message =
+          "Password reset has been initiated. Check You email for further instruction. ";
+        this.successMessage = res["message"] || message;
+        // this.router.navigate(["/home"]);
+      },
+      error => this.onGettingResponseError(error, form)
+    );
+  }
 
   submitPasswordReset(form: FormGroup) {
     if (form.invalid) {
@@ -61,13 +82,47 @@ export class PasswordComponent implements OnInit {
         form.controls.confirmPassword.value
       );
     } catch (error) {
+      this.errorMessage = error;
       console.error(error);
+      return;
     }
+
+    form.disable();
+    this._passwordService.resetPassword(form.value).subscribe(
+      res => {
+        const message =
+          "Password has been reset sucessfully.You can login with new your Password. ";
+        this.successMessage = res["message"] || message;
+      },
+      error => this.onGettingResponseError(error, form)
+    );
+  }
+
+  private onGettingResponseError(error: HttpErrorResponse, form: FormGroup) {
+    this.errorMessage = error.error.message;
+    form.enable();
+  }
+
+  private validateUrl() {
+    this._activatedRoute.params.subscribe(res => {
+      if (res.id !== "request" && res.id !== "reset") {
+        return this.router.navigate(["/home"]);
+      }
+      if (res.id === "reset") {
+        this._activatedRoute.queryParams.subscribe(params => {
+          if (!params.token) {
+            return this.router.navigate(["/home"]);
+          }
+          this.token = params.token;
+        });
+      }
+      this.uiType = res.id;
+    });
   }
 
   private initializeForms() {
     this.passwordRequestForm = this.fb.group({
-      emailId: ["", Validators.required, Validators.email]
+      email: ["", [Validators.required, Validators.email]]
     });
 
     this.passwordResetForm = this.fb.group({
