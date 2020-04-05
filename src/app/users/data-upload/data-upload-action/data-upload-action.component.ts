703a1b021985e05c58eb402629ca6f87e810a49f
anirudh <anirudh.chauhan@dhwaniris.com>
2020-04-05 17:52:00 +0530

    :sparkles: mapped url to every form control in finance data upload component

--- src/app/users/data-upload/data-upload-action/data-upload-action.component.ts
+++ src/app/users/data-upload/data-upload-action/data-upload-action.component.ts
@@ -2,45 +2,67 @@ import {Component, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
 import {ActivatedRoute, Router} from '@angular/router';
 import {FinancialDataService} from '../../services/financial-data.service';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
-  selector: 'app-data-upload-action',
-  templateUrl: './data-upload-action.component.html',
-  styleUrls: ['./data-upload-action.component.scss']
+    selector: 'app-data-upload-action',
+    templateUrl: './data-upload-action.component.html',
+    styleUrls: ['./data-upload-action.component.scss']
 })
 export class DataUploadActionComponent implements OnInit {
 
-  financialYearDropdown = [
-    {id: '2015-16', itemName: '2015-16'},
-    {id: '2016-17', itemName: '2016-17'}
-    // {id: '2017-18', itemName: '2017-18'}
-  ];
-  auditStatusDropdown = [{
-    id: 'audited',
-    itemName: 'Audited'
-  }, {
-    id: 'unaudited',
-    itemName: 'Unaudited'
-  }];
+    financialYearDropdown = [
+        {id: '2015-16', itemName: '2015-16'},
+        {id: '2016-17', itemName: '2016-17'},
+        {id: '2018-19', itemName: '2018-19'},
+        {id: '2019-20', itemName: '2019-2020'},
+    ];
+    auditStatusDropdown = [{
+        id: 'audited',
+        itemName: 'Audited'
+    }, {
+        id: 'unaudited',
+        itemName: 'Unaudited'
+    }];
 
-  constructor(public financeDataService: FinancialDataService, public location: Location, private activatedRoute: ActivatedRoute, private router: Router) {
+    financialUploadForm: FormGroup;
 
-  }
+    constructor(public financeDataService: FinancialDataService,
+                public location: Location,
+                private fb: FormBuilder,
+                private activatedRoute: ActivatedRoute, private router: Router) {
+        this.financialUploadForm = this.fb.group({
+            audited: new FormControl({value: null, disabled: true}),
+            financialYear: new FormControl(null)
+        });
 
-  ngOnInit() {
-    if (!this.financeDataService.selectedFinancialRequest) {
-      this.location.back();
     }
-    console.log(this.financeDataService.selectedFinancialRequest);
-  }
 
-  fileButtonClickHandler(...args) {
-    let urlObject = this.financeDataService.selectedFinancialRequest;
-    args.map(key => urlObject = urlObject[key]);
-    if (urlObject) {
-      if (typeof urlObject === 'string') {
-        window.open(urlObject);
-      }
+    ngOnInit() {
+        if (!this.financeDataService.selectedFinancialRequest) {
+            this.location.back();
+        } else {
+            const {financialYear, audited} = this.financeDataService.selectedFinancialRequest;
+            const selectedFinancialYearObject = this.financialYearDropdown.filter((item) => item.id === financialYear);
+            if (selectedFinancialYearObject) {
+                this.financialUploadForm.controls['financialYear'].setValue(selectedFinancialYearObject);
+            }
+            if (audited) {
+                this.financialUploadForm.controls['audited'].setValue([this.auditStatusDropdown[0]]);
+            } else {
+                this.financialUploadForm.controls['audited'].setValue([this.auditStatusDropdown[1]]);
+            }
+            console.log(selectedFinancialYearObject);
+        }
     }
-  }
+
+    fileButtonClickHandler(...args) {
+        let urlObject = this.financeDataService.selectedFinancialRequest;
+        args.map(key => urlObject = urlObject[key]);
+        if (urlObject) {
+            if (typeof urlObject === 'string') {
+                window.open(urlObject);
+            }
+        }
+    }
 }
--- src/app/users/data-upload/data-upload.module.ts
+++ src/app/users/data-upload/data-upload.module.ts
@@ -4,14 +4,16 @@ import {CommonModule} from '@angular/common';
 import {DataUploadRoutingModule} from './data-upload-routing.module';
 import { DataUploadActionComponent } from './data-upload-action/data-upload-action.component';
 import {AngularMultiSelectModule} from 'angular2-multiselect-dropdown';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DataUploadRoutingModule,
-    AngularMultiSelectModule,
-  ],
+    imports: [
+        CommonModule,
+        DataUploadRoutingModule,
+        AngularMultiSelectModule,
+        ReactiveFormsModule,
+    ],
   declarations: [DataUploadActionComponent],
 
 })
