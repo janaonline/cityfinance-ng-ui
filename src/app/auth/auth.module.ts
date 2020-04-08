2ae4e0aeed14b5f73fcb5e6fc3fdaf88ac46e9ac
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-04-08 16:48:04 +0530

    :sparkle: :boom: :lipstick: Shifted the ulb dropdown to table component

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,22 +1,28 @@
-import {CommonModule} from '@angular/common';
-import {NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  MatAutocompleteModule,
+  MatDialogModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTooltipModule,
+} from '@angular/material';
+import { AngularMultiSelectModule } from 'angular2-multiselect-dropdown';
 
-import {AngularMaterialModule} from '../angular-material.module';
-import {CfChartsModule} from '../shared/cf-charts/cf-charts.module';
-import {AuthRouter} from './auth.router';
-import {AuthService} from './auth.service';
-import {HomeHeaderComponent} from './home-header/home-header.component';
-import {HeatMapComponent} from './home/heat-map/heat-map.component';
-import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
-import {StatisticsComponent} from './statistics/statistics.component';
-import {HomeTabViewComponent} from './home/home-tab-view/home-tab-view.component';
-import {AngularMultiSelectModule} from 'angular2-multiselect-dropdown';
-import {SharedModule} from '../shared/shared.module';
-import {UlbCoverageComponent} from './home/home-tab-view/ulb-coverage/ulb-coverage.component';
-import {MatDialogModule, MatTooltipModule} from '@angular/material';
+import { AngularMaterialModule } from '../angular-material.module';
+import { CfChartsModule } from '../shared/cf-charts/cf-charts.module';
+import { SharedModule } from '../shared/shared.module';
+import { AuthRouter } from './auth.router';
+import { AuthService } from './auth.service';
+import { HomeHeaderComponent } from './home-header/home-header.component';
+import { HeatMapComponent } from './home/heat-map/heat-map.component';
+import { HomeTabViewComponent } from './home/home-tab-view/home-tab-view.component';
+import { UlbCoverageComponent } from './home/home-tab-view/ulb-coverage/ulb-coverage.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
 // import { GridComponent } from './home/grid/grid.component';
 @NgModule({
@@ -31,7 +37,12 @@ import {MatDialogModule, MatTooltipModule} from '@angular/material';
     CommonModule,
     AngularMultiSelectModule,
     MatTooltipModule,
-    MatDialogModule
+    MatDialogModule,
+
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatInputModule
   ],
   providers: [AuthService],
   declarations: [
@@ -48,5 +59,4 @@ import {MatDialogModule, MatTooltipModule} from '@angular/material';
   exports: [HomeHeaderComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AuthModule {
-}
+export class AuthModule {}
