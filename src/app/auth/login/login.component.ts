061fae9828c22acefd33e59c022ed9ddbab24300
anirudh <anirudh.chauhan@dhwaniris.com>
2020-04-09 11:36:09 +0530

    changes with feature/random

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/auth/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {AuthService} from 'src/app/auth/auth.service';
 
-import { environment } from './../../../environments/environment';
+import {environment} from './../../../environments/environment';
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.scss"]
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
@@ -16,7 +16,7 @@ export class LoginComponent implements OnInit {
   public submitted = false;
   public formError: boolean;
   public creditRatingReportUrl =
-    environment.api.url + "assets/credit_rating.xlsx";
+    environment.api.url + 'assets/credit_rating.xlsx';
 
   public loginError: string;
 
@@ -24,18 +24,20 @@ export class LoginComponent implements OnInit {
     private fb: FormBuilder,
     private authService: AuthService,
     private router: Router
-  ) {}
+  ) {
+  }
 
   ngOnInit() {
     this.loginForm = this.fb.group({
-      email: ["", Validators.required],
-      password: ["", Validators.required]
+      email: ['', Validators.required],
+      password: ['', Validators.required]
     });
 
     this.authService.badCredentials.subscribe(res => {
       this.badCredentials = res;
     });
   }
+
   get lf() {
     return this.loginForm.controls;
   }
@@ -46,16 +48,16 @@ export class LoginComponent implements OnInit {
     if (this.loginForm.valid) {
       this.authService.signin(this.loginForm.value).subscribe(
         res => {
-          if (res && res["token"]) {
-            localStorage.setItem("id_token", JSON.stringify(res["token"]));
-            localStorage.setItem("userData", JSON.stringify(res["user"]));
-            this.router.navigate(["home"]);
+          if (res && res['token']) {
+            localStorage.setItem('id_token', JSON.stringify(res['token']));
+            localStorage.setItem('userData', JSON.stringify(res['user']));
+            this.router.navigate(['home']);
           } else {
-            localStorage.removeItem("id_token");
+            localStorage.removeItem('id_token');
           }
         },
         error => {
-          this.loginError = error.error["msg"] || "Server Error";
+          this.loginError = error.error['msg'] || 'Server Error';
           console.error(error);
         }
       );
