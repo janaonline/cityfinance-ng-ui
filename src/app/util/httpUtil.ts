8fe8c61b75d8d5ecad19f72bdf1d5d4b65d553df
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-04-10 17:21:52 +0530

    :boom: Create multipel utility class

---
+++ src/app/util/httpUtil.ts
@@ -0,0 +1,12 @@
+import { HttpParams } from '@angular/common/http';
+
+export class HttpUtility {
+  public convertToHttpParams(params: { [key: string]: string }) {
+    let queryParams = new HttpParams();
+    // tslint:disable-next-line: forin
+    for (const key in params) {
+      queryParams = queryParams.set(key, params[key]);
+    }
+    return queryParams;
+  }
+}
