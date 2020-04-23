75a21b0652783647b0ed78a633df42ee83ee2a35
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-04-23 11:21:37 +0530

    :bug: Fixed sessionId not being send everytime

--- src/app/security/custom-http.interceptor.ts
+++ src/app/security/custom-http.interceptor.ts
@@ -1,65 +1,72 @@
-import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpErrorResponse} from '@angular/common/http';
-import {Injectable} from '@angular/core';
-import {Observable, of, Subject, throwError} from 'rxjs';
-import {NavigationEnd, ResolveEnd, Router} from '@angular/router';
-import {catchError, filter, takeUntil} from 'rxjs/operators';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NavigationEnd, ResolveEnd, Router } from '@angular/router';
+import { Observable, Subject, throwError } from 'rxjs';
+import { catchError, filter, takeUntil } from 'rxjs/operators';
 
 @Injectable()
 export class CustomHttpInterceptor implements HttpInterceptor {
-
-
   routerNavigationSuccess = new Subject<any>();
 
-  constructor(private router: Router
-    , private _router: Router) {
+  constructor(private router: Router, private _router: Router) {
     this.initializeRequestCancelProccess();
   }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    if (req.body instanceof File && req.method === 'PUT') {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (req.body instanceof File && req.method === "PUT") {
       return next.handle(req);
     }
 
-    const token = JSON.parse(localStorage.getItem('id_token'));
-    const sessionID = sessionStorage.getItem('sessionID');
+    const token = JSON.parse(localStorage.getItem("id_token"));
+    const sessionID = sessionStorage.getItem("sessionID");
     let headers = req.headers;
-    if (!req.headers.has('Accept')) {
-      headers = req.headers.set('Content-Type', 'application/json');
+
+    if (!req.headers.has("Accept")) {
+      headers = req.headers.set("Content-Type", "application/json");
     }
+    console.log(`sessionID`, sessionID, req.url);
     if (sessionID) {
-      headers = headers.set('sessionId', sessionID);
+      headers = headers.set("sessionId", sessionID);
     }
     if (token) {
-      headers = headers.set('x-access-token', token);
-      const authReq = req.clone({headers});
-      return next.handle(authReq).pipe(
+      headers = headers.set("x-access-token", token);
+    }
+    const authReq = req.clone({ headers });
+    return next
+      .handle(authReq)
+      .pipe(
         takeUntil(this.routerNavigationSuccess),
         catchError(this.handleError)
       );
-    }
-
-    return next.handle(req).pipe(
-      takeUntil(this.routerNavigationSuccess),
-      catchError(this.handleError)
-    );
   }
 
   initializeRequestCancelProccess() {
     this._router.events
       .pipe(
-        filter(event => event instanceof ResolveEnd || event instanceof NavigationEnd),
+        filter(
+          (event) =>
+            event instanceof ResolveEnd || event instanceof NavigationEnd
+        ),
         filter((event: ResolveEnd | NavigationEnd) => {
-          return event.url.split('?')[0] !== event.urlAfterRedirects.split('?')[0];
+          return (
+            event.url.split("?")[0] !== event.urlAfterRedirects.split("?")[0]
+          );
         }),
-        filter(event => event instanceof NavigationEnd)
+        filter((event) => event instanceof NavigationEnd)
       )
       .subscribe(this.routerNavigationSuccess);
   }
 
-
   private handleError = (err: HttpErrorResponse) => {
+    /**
+     * @description 401 means usre need to be logged in to access this api. Therefore, redirect the user
+     * to login page
+     */
     if (err.status === 401) {
-      this.router.navigate(['login']);
+      this.router.navigate(["login"]);
     }
     return throwError(err);
   };
