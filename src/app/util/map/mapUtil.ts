fbb40a65a71d6b3993d7fff163cabbe9a831c000
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-05-06 14:43:59 +0530

    Implemented new state centroids

--- src/app/util/map/mapUtil.ts
+++ src/app/util/map/mapUtil.ts
@@ -23,6 +23,21 @@ export class MapUtil {
     keyboard: false,
   };
 
+  /**
+   * @description At India Map, for some states, the centroid value we get by either calculation
+   * or by using leaflet <code> getBound().getCenter() </code> does not lie
+   * within the state boundry.Therefore, we need to statically set centroid co ordinates for
+   * those states.
+   */
+  private static readonly customStateCentroids = {
+    Kerala: { lat: 9.999675, lng: 77.199765 },
+    Punjab: { lat: 31.632808, lng: 75.976851 },
+    Goa: { lat: 15.441705, lng: 74.699032 },
+    Haryana: { lat: 29.501121, lng: 76.180837 },
+    Delhi: { lat: 28.689453, lng: 77.814074 },
+    "Himachal Pradesh": { lat: 31.747344, lng: 78.364865 },
+  };
+
   public static getStateName(layer: ILeafletStateClickEvent | L.Layer): string {
     return layer instanceof L.Layer
       ? (<any>layer).feature.properties.ST_NM
@@ -63,6 +78,18 @@ export class MapUtil {
     map = MapUtil.centerMap(map, stateLayers);
 
     return { map, stateLayers };
+  }
+
+  public static getStateCentroid(
+    layer: ILeafletStateClickEvent | L.Layer
+  ): { lat: number; lng: number } {
+    const stateName = MapUtil.getStateName(layer);
+    if (!stateName) {
+      return null;
+    }
+    return MapUtil.customStateCentroids[stateName]
+      ? MapUtil.customStateCentroids[stateName]
+      : (<any>layer).getBounds().getCenter();
   }
 
   private static applyDefaultStateColor(
