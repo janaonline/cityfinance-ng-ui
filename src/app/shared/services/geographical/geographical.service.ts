107a87337bc2b2c9a71d5eda961d524cea42c509
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-05-18 09:46:34 +0530

    Replaced old fetch service with httpClient

--- src/app/shared/services/geographical/geographical.service.ts
+++ src/app/shared/services/geographical/geographical.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { IRawIndiaGEOData } from 'src/app/models/geoDatas/india';
 import * as topo from 'topojson';
@@ -12,20 +11,7 @@ export class GeographicalService {
   constructor(private _http: HttpClient) {}
 
   loadRawIndiaGeoData() {
-    return <Observable<IRawIndiaGEOData>>Observable.create(
-      async (observer: Observer<{}>) => {
-        try {
-          const rawMapData = await (
-            await fetch("/assets/jsonFile/india_v2.json")
-          ).json();
-
-          observer.next(rawMapData);
-          observer.complete();
-        } catch (error) {
-          observer.error(error);
-        }
-      }
-    );
+    return this._http.get<IRawIndiaGEOData>("/assets/jsonFile/india_v2.json");
   }
 
   loadConvertedIndiaGeoData() {
