e43083115490e7897fd407c6681bbcda221024ff
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-06-05 18:02:24 +0530

    :lipstick: Changed the error show

--- src/app/pages/questionnaires/state/property-tax/property-tax.component.ts
+++ src/app/pages/questionnaires/state/property-tax/property-tax.component.ts
@@ -22,6 +22,8 @@ export class PropertyTaxComponent implements OnInit, OnChanges {
 
   todayDate = new Date();
 
+  clickedonNext = false;
+
   constructor(private _fb: FormBuilder) {
     this.initializeForm();
   }
@@ -49,6 +51,22 @@ export class PropertyTaxComponent implements OnInit, OnChanges {
         });
         this.questionForm.updateValueAndValidity();
       });
+  }
+
+  onClickNext() {
+    this.clickedonNext = true;
+    if (this.questionForm.invalid) {
+      return;
+    }
+    this.answer.emit(this.questionForm.value);
+  }
+
+  public GetFormControlErrors(controlName: string) {
+    return !!(
+      this.clickedonNext && this.questionForm.controls[controlName].errors
+    )
+      ? this.questionForm.controls[controlName].errors
+      : null;
   }
 
   private initializeForm() {
@@ -257,7 +275,6 @@ export class PropertyTaxComponent implements OnInit, OnChanges {
     if (!dependentControl || dependentControl.value !== "No") {
       return null;
     }
-    console.log(`Action_Date_PT_B `, control.value);
 
     if (control.value) {
       return null;
