f494d553535f2fd397b0ebbc2ad8aac51763d48d
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-06-08 13:05:02 +0530

    :lipstick: Changed the questions

--- src/app/pages/questionnaires/state/configs/document.config.ts
+++ src/app/pages/questionnaires/state/configs/document.config.ts
@@ -4,13 +4,13 @@ import { IQuestionnaireDocumentsCollection } from '../../model/document-collecti
 
 const QuestionsIdMapping = {
   State_Acts_Doc:
-    "Latest copies of the State Municipal Corporation Act and State Municipalities Act along with copies of amendments pertaining to provisions of the property tax and/or user charges (Please highlight the sections relevant to property tax and user charges if possible)",
+    "Latest copies of the <strong>State Municipal Corporation Act and State Municipalities Act or City specific Acts </strong> along with copies of <strong>amendments</strong> pertaining to provisions of the property tax and/or user charges (Please highlight the sections relevant to property tax and user charges if possible)",
   State_Rules_Doc:
-    "Latest copies of the Notified Rules and any amendments therein that pertain to property tax and/or user charges",
+    "Latest copies of the Notified Rules and amendments therein that pertain to <strong> property tax</strong>",
   Admin_Doc:
-    "Other Acts/bye-laws/Rules that govern the administration of property tax and/or user charges",
+    "Latest copies of the Notified Rules and amendments therein that pertain to <strong>user charges</strong>",
   Implement_Doc:
-    "PPT/Word file/PDF doc containing the implementation plan for meeting the conditions of the scheme",
+    "PPT/Word file/PDF doc containing the <strong>implementation plan</strong> for meeting the conditions of the scheme",
   Other_Doc: "Other documents relevant to the questionnaire",
 };
 
