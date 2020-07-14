62eb6cc043fc6f36aadaa354a3a27be1b19ffe84
Shadab Faiz <Shadab.faiz@dhwaniris.com>
2020-07-14 16:49:45 +0530

    :sparkle: Added Google Tag Manager

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy } from '@angular/core';
 import { delay } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 import { GlobalLoaderService } from './shared/services/loaders/global-loader.service';
 import { SessionService } from './shared/services/session/session.service';
@@ -7,35 +8,55 @@ import { SessionService } from './shared/services/session/session.service';
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
-  styleUrls: ["./app.component.scss"]
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent implements OnDestroy {
-  title = "PerfectAngular";
+  title = "City Finance";
 
   showLoader = false;
   sessionId: string;
 
-  constructor(public globalLoader: GlobalLoaderService, private sessionService: SessionService) {
+  constructor(
+    public globalLoader: GlobalLoaderService,
+    private sessionService: SessionService
+  ) {
     this.startSession();
     this.globalLoader
       .observerLoading()
       .pipe(delay(100))
-      .subscribe(loadingStatus => {
+      .subscribe((loadingStatus) => {
         this.showLoader = loadingStatus;
       });
+    this.addCustomScripts();
   }
 
+  private addCustomScripts() {
+    const id = environment.GoogleTagID;
+    if (!id) return false;
+    const scriptTag = document.createElement("script");
+    scriptTag.src = `https://www.googletagmanager.com/gtag/js?id=${id}`;
+    scriptTag.async = true;
+    scriptTag.onload = this.onGoogleTagLoad;
+    document.getElementsByTagName("head")[0].appendChild(scriptTag);
+  }
 
+  onGoogleTagLoad = () => {
+    (<any>window).dataLayer = (<any>window).dataLayer || [];
+    function gtag(...args: any[]) {
+      (<any>dataLayer).push(arguments);
+    }
+    gtag("js", new Date());
+
+    gtag("config", "UA-171288029-1");
+  };
 
   private startSession() {
-    this.sessionService.generateSessionID().subscribe(res => {
-      this.sessionId = res['data']._id;
-    })
+    this.sessionService.generateSessionID().subscribe((res) => {
+      this.sessionId = res["data"]._id;
+    });
   }
 
   ngOnDestroy(): void {
-    this.sessionService.endSession(this.sessionId).subscribe(res => {
-    
-   })
-   }
+    this.sessionService.endSession(this.sessionId).subscribe((res) => {});
+  }
 }
