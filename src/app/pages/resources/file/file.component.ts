2bbb1e32aa78f74b30e324c12e8edb9962766665
Abhidhwani <abhinav.maurya@dhwaniris.com>
2020-09-14 11:46:07 +0530

    500 internal error resolve

--- src/app/pages/resources/file/file.component.ts
+++ src/app/pages/resources/file/file.component.ts
@@ -11,7 +11,7 @@ import { PublicFile } from '../models/fileList';
 
 export class FileComponent implements OnInit {
   files: PublicFile[];
-
+  window = window;
   constructor(private commonService: CommonService) {}
 
   ngOnInit() {
@@ -24,4 +24,4 @@ export class FileComponent implements OnInit {
   }
 };
 
-//window = window;
+ 
