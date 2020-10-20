a4e17ce5fd026d5250bff2873f7563d3a6794255
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-10-20 11:16:12 +0530

    :sparkle: Implemented solid-waste management UI

--- src/app/users/data-upload/components/financial-uploads/financial-uploads.component.ts
+++ src/app/users/data-upload/components/financial-uploads/financial-uploads.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatHorizontalStepper } from '@angular/material';
+import { USER_TYPE } from 'src/app/models/user/userType';
+import { IQuestionnaireResponse } from 'src/app/pages/questionnaires/model/questionnaireResponse.interface';
+
+import { IFinancialData } from '../../models/financial-data.interface';
+import { SolidWasteEmitValue } from '../../models/solid-waste-questions.interface';
 
 @Component({
-  selector: 'app-financial-uploads',
-  templateUrl: './financial-uploads.component.html',
-  styleUrls: ['./financial-uploads.component.scss']
+  selector: "app-financial-uploads",
+  templateUrl: "./financial-uploads.component.html",
+  styleUrls: ["./financial-uploads.component.scss"],
 })
 export class FinancialUploadsComponent implements OnInit {
+  USER_TYPE = USER_TYPE;
+  @ViewChild(MatHorizontalStepper) stepper: MatHorizontalStepper;
 
-  constructor() { }
+  documentData: IQuestionnaireResponse["data"][0]["documents"];
 
-  ngOnInit() {
+  financialData: IFinancialData;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  saveAsDraft() {}
+
+  onFileUploaded(event) {
+    console.log(event);
   }
 
+  onSolidWasteEmit(event: SolidWasteEmitValue) {
+    if (!this.financialData) this.financialData = {} as IFinancialData;
+    this.financialData.solidWasteManagement = {
+      documents: event as Required<SolidWasteEmitValue>,
+    };
+    this.stepper.next();
+  }
 }
