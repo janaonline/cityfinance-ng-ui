f0b7b0b41cdd6214a9351aad4a20e92b2b288683
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-11-04 15:30:51 +0530

    :boom: Updated the validations on form

--- src/app/users/data-upload/components/configs/water-waste-management.ts
+++ src/app/users/data-upload/components/configs/water-waste-management.ts
@@ -62,7 +62,7 @@ services.forEach((service) => {
         new FormControl("", {
           validators: [
             Validators.required,
-            Validators.pattern("^\\d*(.{0,1}\\d{2,2}){0,1}$"),
+            Validators.pattern("^\\d*.{0,1}\\d*$"),
             service.customValidator,
           ],
           updateOn: "blur",
@@ -74,7 +74,7 @@ services.forEach((service) => {
         new FormControl("", {
           validators: [
             Validators.required,
-            Validators.pattern("^\\d*(.{0,1}\\d{2,2}){0,1}$"),
+            Validators.pattern("^\\d*.{0,1}\\d*$"),
           ],
           updateOn: "blur",
         })
@@ -90,7 +90,7 @@ services.forEach((service) => {
         {
           validators: [
             Validators.required,
-            Validators.pattern("^\\d*(.{0,1}\\d{2,2}){0,1}$"),
+            Validators.pattern("^\\d*.{0,1}\\d*$"),
             service.customValidator,
           ],
           updateOn: "blur",
@@ -105,7 +105,7 @@ services.forEach((service) => {
         {
           validators: [
             Validators.required,
-            Validators.pattern("^\\d*(.{0,1}\\d{2,2}){0,1}$"),
+            Validators.pattern("^\\d*.{0,1}\\d*$"),
           ],
           updateOn: "blur",
         },
