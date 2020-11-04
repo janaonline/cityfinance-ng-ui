14c276441576d0411974ec1006e47cd943faf589
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-11-04 18:01:11 +0530

    :lipstick: Added loader

--- src/app/pages/fc-grant/fc-grant.module.ts
+++ src/app/pages/fc-grant/fc-grant.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 import { FcGrantComponent } from './components/fc-grant/fc-grant.component';
 import { FcGrantRoutingModule } from './fc-grant-routing.module';
 
 @NgModule({
-  imports: [CommonModule, FcGrantRoutingModule],
+  imports: [CommonModule, FcGrantRoutingModule, SharedModule],
   declarations: [FcGrantComponent],
 })
 export class FcGrantModule {}
