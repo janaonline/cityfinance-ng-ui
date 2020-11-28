098743dbfc3bdf573771dbb1336793814b2b494b
Shadab Faiz <shadab.faiz@dhwaniris.com>
2020-11-28 17:06:16 +0530

    :boom: Added filter trim

--- src/app/util/httpUtil.ts
+++ src/app/util/httpUtil.ts
@@ -6,7 +6,10 @@ export class HttpUtility {
     // tslint:disable-next-line: forin
     for (const key in params) {
       if (params[key]) {
-        queryParams = queryParams.set(key, params[key]);
+        queryParams = queryParams.set(
+          key,
+          typeof params[key] === "string" ? params[key].trim() : params[key]
+        );
       }
     }
     return queryParams;
