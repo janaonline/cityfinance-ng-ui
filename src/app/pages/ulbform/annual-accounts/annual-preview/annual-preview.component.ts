24ada62f26953e219fe33b99dd95e34886683b72
Pramod <impkagrahari@gmail.com>
2021-05-06 14:41:01 +0530

    pull

--- src/app/pages/ulbform/annual-accounts/annual-preview/annual-preview.component.ts
+++ src/app/pages/ulbform/annual-accounts/annual-preview/annual-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-annual-preview',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnualPreviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _matDialog: MatDialog) { }
 
   ngOnInit(): void {
   }
