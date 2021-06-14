7939673a119aabe80d8f2039e6c0e7936987cb04
Pramod <impkagrahari@gmail.com>
2021-06-14 18:42:32 +0530

    fix error

--- src/app/pages/stateforms/water-rejenuvation/water-rejenuvation-preview/water-rejenuvation-preview.component.ts
+++ src/app/pages/stateforms/water-rejenuvation/water-rejenuvation-preview/water-rejenuvation-preview.component.ts
@@ -18,6 +18,7 @@ import { Router, Event } from "@angular/router";
 import { SweetAlert } from "sweetalert/typings/core";
 const swal: SweetAlert = require("sweetalert");
 import { WaterRejenuvationService } from "../water-rejenuvation.service";
+import { QuestionnaireService } from "src/app/pages/questionnaires/service/questionnaire.service";
 @Component({
   selector: "app-water-rejenuvation-preview",
   templateUrl: "./water-rejenuvation-preview.component.html",
@@ -28,9 +29,57 @@ export class WaterRejenuvationPreviewComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: any,
     private _matDialog: MatDialog,
     public _router: Router,
+    private _questionnaireService: QuestionnaireService,
     public waterRejenuvationService: WaterRejenuvationService
   ) {}
+  styleForPDF = `<style>
+  .header-p {
+    background-color: #047474;
+    height: 70px;
+    text-align: center;
+}
+.heading-p {
+    color: #FFFFFF;
+    font-size: 18px;
+    padding-top: 1.6rem !important;
+    font-weight: 700;
+
+}
+
+.card {
+    margin-top: 10px !important;
+    padding: 5px 10px;
+    background-color: #EBF5F5;
+}
+
+.qus-h {
+    margin-bottom: .5rem;
+    margin-top: .5rem;
+    font-size: 10px !important;
+}
+
+.ans-h {
+    margin-bottom: .5rem;
+    margin-left: 1.2rem;
+    margin-top: .5rem;
+    font-size: 10px !important;
+}
+.m-r{
+margin-bottom: 1.5rem !important;
+}
+
+.h-cls{
+      display: none;
+    }
+.form-status {
+      font-size: 10px;
+      margin-top: 10px;
+    }
+
+  </style>`
+  @ViewChild("waterRe") _html: ElementRef;
   @ViewChild("template") template;
+  showLoader;
   dialogRef;
   err = "";
   status;
@@ -40,57 +89,57 @@ export class WaterRejenuvationPreviewComponent implements OnInit {
     console.log(this.data);
   }
 
-  clickedDownloadAsPDF() {
-    let change = sessionStorage.getItem("changeInWaterRejenuvation");
-    if (change == "true") {
-      this.openModal(this.template);
-    } else {
-      // this.downloadAsPDF();
-    }
-  }
+  // clickedDownloadAsPDF() {
+  //   let change = sessionStorage.getItem("changeInWaterRejenuvation");
+  //   if (change == "true") {
+  //     this.openModal(this.template);
+  //   } else {
+  //     // this.downloadAsPDF();
+  //   }
+  // }
 
   close() {
     this._matDialog.closeAll();
   }
 
-  // downloadAsPDF() {
-  //   const elementToAddPDFInString = this._html.nativeElement.outerHTML;
-  //   const html = this.styleForPDF + elementToAddPDFInString;
-  //   this.showLoader = true;
-  //   this._questionnaireService.downloadPDF({ html }).subscribe(
-  //     (res) => {
-  //       this.downloadFile(res.slice(0), "pdf", "gtcertificate.pdf");
-  //       this.showLoader = false;
-  //     },
-  //     (err) => {
-  //       this.showLoader = false;
-  //       this.onGettingError(
-  //         ' "Failed to download PDF. Please try after sometime."'
-  //       );
-  //     }
-  //   );
-  // }
-  // private onGettingError(message: string) {
-  //   const option = { ...defaultDailogConfiuration };
-  //   option.buttons.cancel.text = "OK";
-  //   option.message = message;
-  //   this.showLoader = false;
-  //   this._matDialog.open(DialogComponent, { data: option });
-  // }
-  // private downloadFile(blob: any, type: string, filename: string): string {
-  //   const url = window.URL.createObjectURL(blob); // <-- work with blob directly
+  downloadAsPDF() {
+    const elementToAddPDFInString = this._html.nativeElement.outerHTML;
+    const html = this.styleForPDF + elementToAddPDFInString;
+    this.showLoader = true;
+    this._questionnaireService.downloadPDF({ html }).subscribe(
+      (res) => {
+        this.downloadFile(res.slice(0), "pdf", "gtcertificate.pdf");
+        this.showLoader = false;
+      },
+      (err) => {
+        this.showLoader = false;
+        this.onGettingError(
+          ' "Failed to download PDF. Please try after sometime."'
+        );
+      }
+    );
+  }
+  private onGettingError(message: string) {
+    const option = { ...defaultDailogConfiuration };
+    option.buttons.cancel.text = "OK";
+    option.message = message;
+    this.showLoader = false;
+    this._matDialog.open(DialogComponent, { data: option });
+  }
+  private downloadFile(blob: any, type: string, filename: string): string {
+    const url = window.URL.createObjectURL(blob); // <-- work with blob directly
 
-  //   // create hidden dom element (so it works in all browsers)
-  //   const a = document.createElement("a");
-  //   a.setAttribute("style", "display:none;");
-  //   document.body.appendChild(a);
+    // create hidden dom element (so it works in all browsers)
+    const a = document.createElement("a");
+    a.setAttribute("style", "display:none;");
+    document.body.appendChild(a);
 
-  //   // create file, attach to hidden element and open hidden element
-  //   a.href = url;
-  //   a.download = filename;
-  //   a.click();
-  //   return url;
-  // }
+    // create file, attach to hidden element and open hidden element
+    a.href = url;
+    a.download = filename;
+    a.click();
+    return url;
+  }
 
   openModal(template: TemplateRef<any>) {
     const dialogConfig = new MatDialogConfig();
