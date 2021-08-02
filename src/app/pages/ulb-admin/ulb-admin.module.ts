52835a702688a1f91ff62b10725cff5d2803a1fd
Pramod <impkagrahari@gmail.com>
2021-08-02 11:24:30 +0530

    fixing bugs

--- src/app/pages/ulb-admin/ulb-admin.module.ts
+++ src/app/pages/ulb-admin/ulb-admin.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
+// import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { ReactiveFormsModule } from '@angular/forms';
 
-import { UlbAdminRoutingModule } from './ulb-admin-routing.module';
-import { UlbReviewComponent } from './ulb-admin/ulb-review.component';
-import { UlbAdminComponent } from './ulb-admin.component';
-import {MatIconModule} from '@angular/material/icon';
-import { NgxPaginationModule } from 'ngx-pagination';
+// import { UlbAdminRoutingModule } from './ulb-admin-routing.module';
+// import { UlbReviewComponent } from './ulb-admin/ulb-review.component';
+// import { UlbAdminComponent } from './ulb-admin.component';
+// import {MatIconModule} from '@angular/material/icon';
+// import { NgxPaginationModule } from 'ngx-pagination';
 
 
-@NgModule({
-  declarations: [
-    UlbReviewComponent,
-    UlbAdminComponent
-  ],
-  imports: [
-    CommonModule,
-    UlbAdminRoutingModule,
-    MatIconModule,
-    ReactiveFormsModule,
-    NgxPaginationModule
-  ]
-})
-export class UlbAdminModule { }
+// @NgModule({
+//   declarations: [
+//     UlbReviewComponent,
+//     UlbAdminComponent
+//   ],
+//   imports: [
+//     CommonModule,
+//     UlbAdminRoutingModule,
+//     MatIconModule,
+//     ReactiveFormsModule,
+//     NgxPaginationModule
+//   ]
+// })
+// export class UlbAdminModule { }
