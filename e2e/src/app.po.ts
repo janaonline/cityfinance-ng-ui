7640da374c70faf99fc5165ca8e0e28a80f79af1
Pramod <impkagrahari@gmail.com>
2021-08-04 18:29:38 +0530

    fixing action bugs

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,24 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('app-home-header a')).getText();
   }
+  get15FCButton(){
+     return element(by.css('[routerlink="/fc-home-page" ]'));
+  }
+  getLogin(){
+   return element(by.css('[value="LOGIN"]'));
+ }
+ getUlbLogin(){
+  return element(by.css('#ulb'));
+}
+getUlbPage(){
+  return element(by.css('[routerlink="/mohua" ]'));
+}
+// getStateLogin(){
+//   return element(by.css('[value="LOGIN"]'));
+// }
+// getMohuaLogin(){
+//   return element(by.css('[value="LOGIN"]'));
+// }
 }
