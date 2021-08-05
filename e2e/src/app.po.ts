0d71a5bf6e7b6178eb00ccd2c373ae9d3a7fcbb8
Pramod <impkagrahari@gmail.com>
2021-08-05 15:10:36 +0530

    testing code removed

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,29 +1,29 @@
-import { browser, by, element } from 'protractor';
+// import { browser, by, element } from 'protractor';
 
-export class AppPage {
-  navigateTo() {
-    return browser.get('/');
-  }
+// export class AppPage {
+//   navigateTo() {
+//     return browser.get('/');
+//   }
 
-  getParagraphText() {
-    return element(by.css('app-home-header a')).getText();
-  }
-  get15FCButton(){
-     return element(by.css('[routerlink="/fc-home-page" ]'));
-  }
-  getLogin(){
-   return element(by.css('[value="LOGIN"]'));
- }
- getUlbLogin(){
-  return element(by.css('#ulb'));
-}
-getUlbPage(){
-  return element(by.css('[routerlink="/mohua" ]'));
-}
-// getStateLogin(){
-//   return element(by.css('[value="LOGIN"]'));
+//   getParagraphText() {
+//     return element(by.css('app-home-header a')).getText();
+//   }
+//   get15FCButton(){
+//      return element(by.css('[routerlink="/fc-home-page" ]'));
+//   }
+//   getLogin(){
+//    return element(by.css('[value="LOGIN"]'));
+//  }
+//  getUlbLogin(){
+//   return element(by.css('#ulb'));
 // }
-// getMohuaLogin(){
-//   return element(by.css('[value="LOGIN"]'));
+// getUlbPage(){
+//   return element(by.css('[routerlink="/mohua" ]'));
 // }
-}
+// // getStateLogin(){
+// //   return element(by.css('[value="LOGIN"]'));
+// // }
+// // getMohuaLogin(){
+// //   return element(by.css('[value="LOGIN"]'));
+// // }
+// }
