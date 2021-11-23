3b9cf2ea586d7c64201d33c5ca604966ab9f9767
Pramod <impkagrahari@gmail.com>
2021-11-23 14:43:15 +0530

    cleaning, language

---
+++ src/app/shared/components/n-home-header/n-home-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-n-home-header',
+  templateUrl: './n-home-header.component.html',
+  styleUrls: ['./n-home-header.component.scss']
+})
+export class NHomeHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
