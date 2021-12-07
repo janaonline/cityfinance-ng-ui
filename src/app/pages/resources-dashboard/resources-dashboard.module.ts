9618d4ad572af0db3c9cb18a1affcbde110fde23
Pramod <impkagrahari@gmail.com>
2021-12-07 18:39:00 +0530

    card-res

--- src/app/pages/resources-dashboard/resources-dashboard.module.ts
+++ src/app/pages/resources-dashboard/resources-dashboard.module.ts
@@ -5,8 +5,12 @@ import { ResourcesDashboardRoutingModule } from './resources-dashboard-routing.m
 import { ResourcesDashboardComponent } from './resources-dashboard.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { LearningCenterComponent } from './learning-center/learning-center.component';
+import { DataSetsComponent } from './data-sets/data-sets.component';
+import { ReportsPublicationComponent } from './reports-publication/reports-publication.component';
+import { LatestNewsComponent } from './latest-news/latest-news.component';
 @NgModule({
-  declarations: [ResourcesDashboardComponent],
+  declarations: [ResourcesDashboardComponent, LearningCenterComponent, DataSetsComponent, ReportsPublicationComponent, LatestNewsComponent],
   imports: [
     CommonModule,
     ResourcesDashboardRoutingModule,
