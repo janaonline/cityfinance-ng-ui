1e2c8dcd1a01cefe9e046a8aff427fe816403720
Ashu Gupta <ashu10832@gmail.com>
2021-12-09 08:25:54 +0530

    build check

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,6 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 let url;
-
 let GoogleTagID: string;
 if (
   window.location.hostname.includes("demo") ||
