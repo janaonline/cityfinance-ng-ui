223c386cf763e65a78227d491cfcb3d0ecf8e543
Abhay Kumar <akindia619@gmail.com>
2021-12-10 13:40:07 +0530

    own-revenue-dashboard - data availability section

--- src/app/pages/own-revenue-dashboard/own-revenue-dashboard.component.ts
+++ src/app/pages/own-revenue-dashboard/own-revenue-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-own-revenue-dashboard',
@@ -13,3 +14,31 @@ export class OwnRevenueDashboardComponent implements OnInit {
   }
 
 }
+
+
+
+const canvas = <HTMLCanvasElement> document.getElementById('myChart');
+const ctx = canvas.getContext('2d');
+const myChart = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue'],
+        datasets: [
+          {
+              label: 'Availability',
+              data: [75],
+              backgroundColor: [
+                  
+              ],
+              borderColor: [
+                  'rgba(255, 99, 132, 1)',
+                  'rgba(54, 162, 235, 1)',
+              ],
+              borderWidth: 1
+          }
+      ]
+    },
+    options: {
+      cutoutPercentage: 90
+    }
+});
