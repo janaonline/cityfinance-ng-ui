760d52595fd1893eaf22574ba97ba2f174ecfa2b
Ashu Gupta <ashu10832@gmail.com>
2021-12-20 17:16:43 +0530

    added 2018-19 in analytics

--- src/app/pages/analytics/home-tab-view/home-tab-view.component.ts
+++ src/app/pages/analytics/home-tab-view/home-tab-view.component.ts
@@ -52,6 +52,7 @@ export class HomeTabViewComponent implements OnInit {
     { id: "2015-16", itemName: "2015-16" },
     { id: "2016-17", itemName: "2016-17" },
     { id: "2017-18", itemName: "2017-18" },
+    { id: "2018-19", itemName: "2018-19" },
   ];
   yearsDropdownSettings = {
     text: "Select Years",
