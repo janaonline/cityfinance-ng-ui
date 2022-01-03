24b9340b4a7100cb27319f9ac133dff18436934b
Ashu Gupta <ashu10832@gmail.com>
2022-01-03 17:17:06 +0530

    resolved bug in action plan

--- src/app/shared/components/ag-grid/ag-grid.component.ts
+++ src/app/shared/components/ag-grid/ag-grid.component.ts
@@ -509,7 +509,7 @@ export class AgGridComponent implements OnInit, OnChanges {
           ? params.data["Funding"].value
           : "",
       valueSetter: syncValueSetter(number),
-      // valueParser: "parseFloat(newValue)",
+      valueParser: "parseInt(newValue)",
       headerName: "% Funding",
       width: 85,
       editable: false,
@@ -774,7 +774,7 @@ export class AgGridComponent implements OnInit, OnChanges {
         this.rowData.yearOutlay[e.rowIndex]["Funding"].value = 0;
       } else {
         this.rowData.yearOutlay[e.rowIndex]["Funding"].value =
-          (e.value / this.rowData.yearOutlay[e.rowIndex]["Cost"].value) * 100;
+          ((e.value / this.rowData.yearOutlay[e.rowIndex]["Cost"].value) * 100).toPrecision(2);
         if (this.rowData.yearOutlay[e.rowIndex]["Funding"].value % 1 != 0) {
           this.rowData.yearOutlay[e.rowIndex]["Funding"].value = (
             (e.value / this.rowData.yearOutlay[e.rowIndex]["Cost"].value) *
@@ -920,7 +920,7 @@ const number = (x, params) => {
   x = Number(parseFloat(x).toFixed(3));
   if (!isNaN(x) && x >= 0 && x < 999999999) {
     if (params.colDef.field == "Cost") return true;
-    return x / 100 < params.data.Cost.value;
+    return x / 100 <= params.data.Cost.value;
   }
   return false;
 };
