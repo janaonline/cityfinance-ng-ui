658ce95a059c0bae86d38cd288d7e49839899661
Abhay Kumar <akindia619@gmail.com>
2022-02-10 11:00:47 +0530

    uncomplete task/ accordion to table ui revamping

--- src/app/shared/components/dashboard-tabs/accordion-to-table/accordion-to-table.component.ts
+++ src/app/shared/components/dashboard-tabs/accordion-to-table/accordion-to-table.component.ts
@@ -9,8 +9,67 @@ import { BorrowingTabService } from '../borrowing-tab.service';
 
 export class AccordionToTableComponent implements OnInit {
   constructor(private borrowingTabService: BorrowingTabService) {}
+  formattedNamesMapping: { [nameIdentifier: string]: string } = {};
+  private regexToSplitWordOnCapitalLetters = /([A-Z]+[^A-Z]*|[^A-Z]+)/;
+  bondIssuerItemData;
+  mainRows;
+  paginatedbondIssuerItem;
 
-  tableView = true;
+
+
+
+  private capitalizedName(originalName: string) {
+    const formattedName = originalName
+      .split(this.regexToSplitWordOnCapitalLetters)
+      .join(" ")
+      .trim();
+
+    return formattedName[0].toUpperCase() + formattedName.substring(1);
+    // formattedName.trim();
+  }
+
+  private onGettingBondIssuerSuccess(res) {
+    Object.keys(res).forEach((name) => {
+      const capitalizedName = this.capitalizedName(name);
+      this.formattedNamesMapping[name] = capitalizedName;
+      res[name].forEach((name) => {
+        const formattedName = this.capitalizedName(name);
+        this.formattedNamesMapping[name] = formattedName;
+      });
+    });
+    this.mainRows = res;
+  }
+
+  private createSubHeader() {
+    let array = [];
+    Object.keys(this.mainRows).forEach((key) => {
+      const config = {
+        text: this.formattedNamesMapping[key],
+        colorWholeRow: true,
+        bold: true,
+        backgroundColor: "F8CBAD",
+      };
+
+      const subHeaderRow = [config];
+
+      array.push(subHeaderRow);
+      const detailRows = this.mainRows[key].map((subRow) => {
+        const detailColumns = {
+          text: this.formattedNamesMapping[subRow],
+        };
+
+        const ulbDataCoulmns = this.bondIssuerItemData.map((ulb) => ({
+          text: ulb[subRow],
+        }));
+        return [detailColumns, ...ulbDataCoulmns];
+      });
+      array = array.concat(detailRows);
+    });
+    return array;
+  }
+
+
+  // tableView = true;
   TableTitles:any;
      HeaderDataOfBorrowTab(){
        this.borrowingTabService.getHeaderName().subscribe(
