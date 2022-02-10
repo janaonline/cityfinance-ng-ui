cdcfa547dcc443ac18c93ab23e78b2139dfdd88f
Abhay Kumar <akindia619@gmail.com>
2022-02-10 18:42:01 +0530

    3rd page of dashboard dropdown of nabar. i.e - slb-dashboard component working

--- src/app/pages/new-dashbords/new-dashbords.module.ts
+++ src/app/pages/new-dashbords/new-dashbords.module.ts
@@ -5,11 +5,13 @@ import { NewDashbordsRoutingModule } from "./new-dashbords-routing.module";
 import { CityComponent } from "./city/city.component";
 import { SharedModule } from "src/app/shared/shared.module";
 import { StateComponent } from './state/state.component';
+import { SlbDashboardComponent } from './slb-dashboard/slb-dashboard.component';
+import { SlbDashboardRoutingModule } from "./slb-dashboard/slb-dashboard-routing.module";
 //import { NationalComponent } from './national/national.component';
 
 
 @NgModule({
-  declarations: [CityComponent, StateComponent],
-  imports: [CommonModule, NewDashbordsRoutingModule, SharedModule],
+  declarations: [CityComponent, StateComponent, SlbDashboardComponent],
+  imports: [CommonModule, NewDashbordsRoutingModule, SharedModule, SlbDashboardRoutingModule ],
 })
 export class NewDashbordsModule {}
---
+++ src/app/pages/new-dashbords/slb-dashboard/slb-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-slb-dashboard',
+  templateUrl: './slb-dashboard.component.html',
+  styleUrls: ['./slb-dashboard.component.css']
+})
+export class SlbDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
