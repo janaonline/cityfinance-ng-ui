fe8ec176a7be90cd58716e5c28974285fca03834
Ashu Gupta <ashu10832@gmail.com>
2022-03-24 10:39:43 +0530

    added own revenu in state db

--- src/app/shared/components/state-filter-data/state-filter-data.component.ts
+++ src/app/shared/components/state-filter-data/state-filter-data.component.ts
@@ -22,7 +22,7 @@ export class StateFilterDataComponent extends BaseComponent implements OnInit {
   filteredOptions: Observable<any[]>;
   lastSelectedId: number = 0;
   ActiveButton: any;
-  filterName: any;
+  filterName = 'revenue';
   tabName: any;
   headOfAccount: any;
   chartId = `stateSCharts-${Math.random()}`;
@@ -275,7 +275,7 @@ export class StateFilterDataComponent extends BaseComponent implements OnInit {
         let mCorporation = res["mCorporation"];
         let tp_data = res["townPanchayat"];
         let m_data = res["municipality"];
-        let natData = res["natAvg"][0]["average"];
+        // let natData = res["natAvg"][0]["average"];
         let stateData = res["stateAvg"][0]["average"];
 
         this.scatterData.data.datasets.forEach((el) => {
