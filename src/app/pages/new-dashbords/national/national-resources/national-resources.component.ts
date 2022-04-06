0af27dc5da2f1d4fd845edebacd99da78323fb5d
chandrashekhar30 <chandra.shekhar@dhwaniris.com>
2022-04-06 18:40:41 +0530

    National Financial Dashboard ->resources->card implementation

--- src/app/pages/new-dashbords/national/national-resources/national-resources.component.ts
+++ src/app/pages/new-dashbords/national/national-resources/national-resources.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { CommonService } from "src/app/shared/services/common.service";
 
 @Component({
   selector: 'app-national-resources',
@@ -7,25 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NationalResourcesComponent implements OnInit {
 
-  constructor() { }
-
-  cardData = [
-    {
-      label: "Digital Property Tax Toolkit",
-      imgUrl: '../../../../assets/new_dashBord_ftr_hdr/shutterstock_546307051/shutterstock_546307051.png',
-      link: ''
-     },
-     {
-      label: "Municipal Borrowing Readiness Toolkit",
-      imgUrl: '../../../../assets/new_dashBord_ftr_hdr/Group 15745/Group 15745.png',
-      link: ''
-     },
-     {
-      label: "Case study on Use of SHG for Property Tax Collection in…",
-      imgUrl: '../../../../assets/new_dashBord_ftr_hdr/Group 15744/Group 15744.png',
-      link: ''
-     },
-  ]
+  constructor(
+    protected _commonService: CommonService,
+    private router: Router
+  ) {
+    this._commonService.getPublicFileList().subscribe((res)=>{
+      this.whatNewData = res
+      console.log(this.whatNewData)
+    })
+  }
+  whatNewData=[]
+  // cardData = [
+  //   {
+  //     label: "Digital Property Tax Toolkit",
+  //     imgUrl: '../../../../assets/new_dashBord_ftr_hdr/shutterstock_546307051/shutterstock_546307051.png',
+  //     link: ''
+  //    },
+  //    {
+  //     label: "Municipal Borrowing Readiness Toolkit",
+  //     imgUrl: '../../../../assets/new_dashBord_ftr_hdr/Group 15745/Group 15745.png',
+  //     link: ''
+  //    },
+  //    {
+  //     label: "Case study on Use of SHG for Property Tax Collection in…",
+  //     imgUrl: '../../../../assets/new_dashBord_ftr_hdr/Group 15744/Group 15744.png',
+  //     link: ''
+  //    },
+  // ]
   ngOnInit(): void {
   }
 
