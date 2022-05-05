7412548cf5e034643cd257a3e80157f65dd70678
Vivek Singh Rajput <vivek.kumar@dhwaniris.com>
2022-05-05 12:58:33 +0530

    :hammer: removed unused animation function

--- src/app/shared/components/revenuechart/revenuechart.component.ts
+++ src/app/shared/components/revenuechart/revenuechart.component.ts
@@ -501,60 +501,6 @@ export class RevenuechartComponent
   }
 
   lastMultipleCharts = [];
-  pieChartAnimation = {
-    duration: 500,
-    easing: 'easeOutQuart',
-    onComplete: function(animation) {
-      console.log('animation', animation)
-        var chartInstance = this.chart;
-        console.log('chartInstance', chartInstance);
-        // const datasets = animation.data.datasets;
-        // console.log('datasets', datasets)
-        console.log('this.data.datasets', this.data.datasets)
-        // var ctx = this.chart.ctx;
-        var ctx = chartInstance.ctx;
-        ctx.font = Chart.helpers.fontString(
-            Chart.defaults.global.defaultFontFamily,
-            'normal',
-            Chart.defaults.global.defaultFontFamily
-        );
-        ctx.textAlign = 'center';
-        ctx.textBaseline = 'bottom';
-        this.data.datasets.forEach(function(dataset) {
-            for (var i = 0; i < dataset.data.length; i++) {
-                var model = dataset._meta[Object.keys(dataset._meta)[0]].data[i]._model,
-                    total = dataset._meta[Object.keys(dataset._meta)[0]].total,
-                    mid_radius = model.innerRadius + (model.outerRadius - model.innerRadius) / 2,
-                    start_angle = model.startAngle,
-                    end_angle = model.endAngle,
-                    mid_angle = start_angle + (end_angle - start_angle) / 2;
-                var x = mid_radius * Math.cos(mid_angle);
-                var y = mid_radius * Math.sin(mid_angle);
-
-                ctx.fillStyle = '#000';
-                if (i == 3) {
-                    // Darker text color for lighter background
-                    ctx.fillStyle = '#444';
-                }
-                var percent = Math.round((dataset.data[i] / total) * 100);
-                //Don't Display If Legend is hide or value is 0
-                console.log('dataset', dataset)
-                if (
-                    dataset.data[i] != 0 &&
-                    percent > 2
-                ) {
-                    ctx.fillText(dataset.data[i], model.x + x, model.y + y);
-
-                    // Display percent in another line, line break doesn't work for fillText
-                    ctx.fillText(percent + '%', model.x + x, model.y + y + 15);
-                }
-            }
-
-        });
-
-    },
-
-  };
 
   createMultipleChart() {
     console.log("multipleDoughnutCharts", this.multipleDoughnutCharts);
