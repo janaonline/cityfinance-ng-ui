739635407c2c28326a7ae88d04c9e413b436c40f
chandra shekhar joshi <chandra.shekhar@dhwaniris.com>
2022-06-21 10:15:04 +0530

    odf form comp

--- src/app/shared2223/components/odf-form/odf-form.component.ts
+++ src/app/shared2223/components/odf-form/odf-form.component.ts
@@ -1,5 +1,6 @@
 import { HttpEventType } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl ,FormGroup, Validators,} from '@angular/forms';
 import { DataEntryService } from 'src/app/dashboard/data-entry/data-entry.service';
 const swal: SweetAlert = require("sweetalert");
 import { SweetAlert } from "sweetalert/typings/core";
@@ -10,13 +11,58 @@ import { SweetAlert } from "sweetalert/typings/core";
 })
 export class OdfFormComponent implements OnInit {
 
-  constructor(private dataEntryService: DataEntryService) { }
-  change = ''
-  submitted = false;
-  filesToUpload: Array<File> = [];
-  filesAlreadyInProcess: number[] = [];
-  millionTiedFileUrl = '';
-  clickedCrossA = false
+  constructor(private dataEntryService: DataEntryService,
+    private fb: FormBuilder) { }
+
+  ratings=[
+  {
+   value:'odf',
+   name:'ODF'
+  },
+  {
+    value:'odf+',
+    name:'ODF+'
+   },
+   {
+    value:'odf++',
+    name:'ODF++'
+   },
+   {
+    value:'water+',
+    name:'Water+'
+   },
+   {
+    value:'nonOdf',
+    name:'Non ODF'
+   },
+   {
+    value:'nonOdf+',
+    name:'Non ODF+'
+   },
+   {
+    value:'nonOdf++',
+    name:'Non ODF++'
+   }]
+   flag = 0;
+   stateActionA = '';
+   stateActionB = '';
+   stateActionC = '';
+   rejectReasonA = null;
+   rejectReasonB = null;
+   millionTiedFileUrl = '';
+   nonMillionTiedFileUrl = '';
+   change=''
+   submitted = false;
+   millionTiedProgress_2021;
+   millionTiedFileUrl_2021 = '';
+   filesToUpload: Array<File> = [];
+   filesAlreadyInProcess: number[] = [];
+   fileProcessingTracker: {
+    [fileIndex: number]: {
+      status: "in-process" | "completed" | "FAILED";
+      message: string;
+    };
+  } = {};
   fileUploadTracker: {
     [fileIndex: number]: {
       alias?: string;
@@ -24,26 +70,51 @@ export class OdfFormComponent implements OnInit {
       status: "in-process" | "FAILED" | "completed";
     };
   } = {};
+  clickedCrossB = false
+  clickedCrossA = false
+  fileName_millionTied = '';
+  millionTiedProgress;
+  fileName_millionTied_2021 = '';
 
-  fileProcessingTracker: {
-    [fileIndex: number]: {
-      status: "in-process" | "completed" | "FAILED";
-      message: string;
-    };
-  } = {};
-  ngOnInit(): void {
+  profileForm = this.fb.group({
+    rating: ['odf', Validators.required],
+    cert: ['', Validators.required],
+    certDate: ['', Validators.required]
+  });
+
+  onSubmit() {
+    console.warn(this.profileForm.value);
   }
   uploadButtonClicked(formName) {
     sessionStorage.setItem("changeInGTC", "true")
     this.change = "true";
   }
   fileChangeEvent(event, progessType, fileName) {
-    console.log(event, fileName)
+    console.log(event)
+    console.log(progessType)
+    console.log(fileName)
+
     this.submitted = false;
     this.resetFileTracker();
     const filesSelected = <Array<File>>event.target["files"];
     this.filesToUpload.push(...this.filterInvalidFilesForUpload(filesSelected));
     this.upload(progessType, fileName);
+  }
+  
+  clearFiles(fileName) {
+    sessionStorage.setItem("changeInGTC", "true")
+    this.change = "true"
+    if (fileName == 'fileName_millionTied') {
+      this.clickedCrossB = true
+      this.millionTiedProgress = '';
+      this.fileName_millionTied = '';
+      this.millionTiedFileUrl = ''
+    }else if(fileName == 'fileName_millionTied_2021'){
+      this.clickedCrossA = true
+      this.millionTiedProgress_2021 = '';
+      this.fileName_millionTied_2021 = '';
+      this.millionTiedFileUrl_2021 = ''
+    }
   }
   resetFileTracker() {
     this.filesToUpload = [];
@@ -51,6 +122,7 @@ export class OdfFormComponent implements OnInit {
     this.fileProcessingTracker = {};
     this.fileUploadTracker = {};
   }
+
   filterInvalidFilesForUpload(filesSelected: File[]) {
     const validFiles = [];
     for (let i = 0; i < filesSelected.length; i++) {
@@ -65,6 +137,7 @@ export class OdfFormComponent implements OnInit {
     }
     return validFiles;
   }
+apiData={}
   async upload(progessType, fileName) {
     const formData: FormData = new FormData();
     const files: Array<File> = this.filesToUpload;
@@ -80,28 +153,19 @@ export class OdfFormComponent implements OnInit {
       this.filesAlreadyInProcess.push(i);
       await this.uploadFile(files[i], i, progessType, fileName);
     }
-
-
   }
-  millionTiedProgress_2021;
-  clearFiles(fileName) {
-    sessionStorage.setItem("changeInGTC", "true")
-   
-    this.change = "true"
-     if(fileName == 'fileName_millionTied_2021'){
-      this.clickedCrossA = true
-      this.millionTiedFileUrl = ''
-      this.millionTiedProgress_2021 = '';
-    }
-    // this.checkDiff();
-  }
+
+  stateActionA_2021 = '';
+  rejectReasonA_2021 = null;
   uploadFile(file: File, fileIndex: number, progessType, fileName) {
     return new Promise((resolve, reject) => {
       this.dataEntryService.getURLForFileUpload(file.name, file.type).subscribe(
         (s3Response) => {
           const fileAlias = s3Response["data"][0]["file_alias"];
+          console.log(fileAlias)
           this[progessType] = Math.floor(Math.random() * 90) + 10;
           const s3URL = s3Response["data"][0].url;
+          console.log(s3URL)
           this.uploadFileToS3(
             file,
             s3URL,
@@ -111,10 +175,15 @@ export class OdfFormComponent implements OnInit {
           );
           resolve("success")
           console.log('file url', fileAlias)
-          // if (fileName === 'fileName_millionTied') {
-          //   this.stateActionA = 'PENDING';
-          //   this.rejectReasonA = null
-          // }
+          console.log(file.name)
+          console.log(file.type)
+          if (fileName === 'fileName_millionTied') {
+            this.stateActionA = 'PENDING';
+            this.rejectReasonA = null
+          } else if (fileName === 'fileName_millionTied_2021') {
+            this.stateActionA_2021 = 'PENDING';
+            this.rejectReasonA_2021 = null
+          }
         },
         (err) => {
           if (!this.fileUploadTracker[fileIndex]) {
@@ -128,7 +197,6 @@ export class OdfFormComponent implements OnInit {
       );
     })
   }
-  millionTiedFileUrl_2021 = '';
   private uploadFileToS3(
     file: File,
     s3URL: string,
@@ -136,25 +204,24 @@ export class OdfFormComponent implements OnInit {
     fileIndex: number,
     progressType: string = ''
   ) {
+    console.log(file)
+    console.log(s3URL)
     this.dataEntryService
       .uploadFileToS3(file, s3URL)
-      // Currently we are not tracking file upload progress. If it is need, uncomment the below code.
-      // .pipe(
-      //   map((response: HttpEvent<any>) =>
-      //     this.logUploadProgess(response, file, fileAlias, fileIndex)
-      //   )
-      // )
       .subscribe(
         (res) => {
+          console.log(res)
           if (res.type === HttpEventType.Response) {
             this[progressType] = 100;
+            console.log(fileAlias)
             if (progressType == 'millionTiedProgress') {
               this.millionTiedFileUrl = fileAlias;
-            }else if (progressType == 'millionTiedProgress_2021') {
+            } else if (progressType == 'nonMillionTiedProgress') {
+              this.nonMillionTiedFileUrl = fileAlias;
+            }  else if (progressType == 'millionTiedProgress_2021') {     
               this.millionTiedFileUrl_2021 = fileAlias;
-            } 
-            // console.log('Progress -', progressType, this.millionTiedFileUrl, this.nonMillionTiedFileUrl, this.nonMillionUntiedFileUrl)
-            // this.checkDiff();
+            }
+            
           }
         },
         (err) => {
@@ -162,4 +229,8 @@ export class OdfFormComponent implements OnInit {
         }
       );
   }
+  ngOnInit(): void {
+  }
+ 
+  
 }
--- src/app/shared2223/shared2223.module.ts
+++ src/app/shared2223/shared2223.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { OdfFormComponent } from "./components/odf-form/odf-form.component";
 import { GfcFormComponent } from "./components/gfc-form/gfc-form.component";
+import { ReactiveFormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [OdfFormComponent, GfcFormComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
   exports: [OdfFormComponent],
 })
 export class Shared2223Module {}
