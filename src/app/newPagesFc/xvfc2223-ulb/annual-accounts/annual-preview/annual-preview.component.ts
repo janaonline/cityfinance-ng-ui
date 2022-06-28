67d9722354c1b66208e82692dce14b72fde139f7
Pramod <impkagrahari@gmail.com>
2022-06-28 15:29:30 +0530

    preview annual acc

--- src/app/newPagesFc/xvfc2223-ulb/annual-accounts/annual-preview/annual-preview.component.ts
+++ src/app/newPagesFc/xvfc2223-ulb/annual-accounts/annual-preview/annual-preview.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { USER_TYPE } from "src/app/models/user/userType";
 @Component({
-  selector: 'app-annual-preview',
-  templateUrl: './annual-preview.component.html',
-  styleUrls: ['./annual-preview.component.scss']
+  selector: "app-annual-preview",
+  templateUrl: "./annual-preview.component.html",
+  styleUrls: ["./annual-preview.component.scss"],
 })
 export class AnnualPreviewComponent implements OnInit {
-
-  constructor() { }
-
+  constructor() {}
+  @ViewChild("annualPreview") _html: ElementRef;
+  @ViewChild("templateAnnual") template;
+  showLoader;
+  ulbName = "";
+  stateName = "";
   ngOnInit(): void {
+    let userData = JSON.parse(localStorage.getItem("userData"));
+    if (userData.role !== USER_TYPE.ULB) {
+      this.ulbName = sessionStorage.getItem("ulbName");
+    } else {
+      this.ulbName = userData["name"];
+    }
+    this.stateName = userData["stateName"];
   }
-
+  closeMat() {}
 }
