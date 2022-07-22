a875c80e1d97e5cfca6f3eaa740e1516fe7a0eb6
chandra shekhar joshi <chandra.shekhar@dhwaniris.com>
2022-07-22 18:24:19 +0530

    conflict

--- src/app/newPagesFc/xvfc2223-state/xvfc2223-state-routing.module.ts
+++ src/app/newPagesFc/xvfc2223-state/xvfc2223-state-routing.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 import { PropertyTaxFloorRateComponent } from './property-tax-floor-rate/property-tax-floor-rate.component';
-=======
-import { PtoComponent } from './pto/pto.component';
 import {ReviewApplicationComponent} from './review-application/review-application.component'
 import { Xvfc2223StateComponent } from "./xvfc2223-state.component";
->>>>>>> f5f745edc3698e4777b8fed5b4873146b8255ac5
 
 const routes: Routes = [
   {
-<<<<<<< HEAD
-    path : "property-tax-floor-rate", component: PropertyTaxFloorRateComponent
-  }
-=======
     path: "",
     component: Xvfc2223StateComponent,
     children: [
       {
-        path: "pto",
-        component: PtoComponent,
+        path: "property-tax-floor-rate",
+        component: PropertyTaxFloorRateComponent,
       },
       {
         path: "review",
@@ -27,7 +19,6 @@ const routes: Routes = [
       },
     ],
   },
->>>>>>> f5f745edc3698e4777b8fed5b4873146b8255ac5
 ];
 
 @NgModule({
