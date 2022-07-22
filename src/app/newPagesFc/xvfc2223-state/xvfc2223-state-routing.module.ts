00452368c37756d5c8c73f3b9545b0cb42e9ecc1
Ashu Gupta <ashu10832@gmail.com>
2022-07-22 14:58:43 +0530

    wip

--- src/app/newPagesFc/xvfc2223-state/xvfc2223-state-routing.module.ts
+++ src/app/newPagesFc/xvfc2223-state/xvfc2223-state-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PtoComponent } from './pto/pto.component';
-
+import {ReviewApplicationComponent} from './review-application/review-application.component'
 const routes: Routes = [
   // {
   //   path : "", component: 
   // },
   {
     path : "pto", component: PtoComponent
+  },
+  {
+    path : "review2223", component: ReviewApplicationComponent
   }
 ];
 
--- src/app/newPagesFc/xvfc2223-state/xvfc2223-state.module.ts
+++ src/app/newPagesFc/xvfc2223-state/xvfc2223-state.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { Xvfc2223StateRoutingModule } from './xvfc2223-state-routing.module';
 import { Xvfc2223StateComponent } from "./xvfc2223-state.component";
 import { PtoComponent } from './pto/pto.component';
-
+import { ReviewApplicationComponent } from './review-application/review-application.component';
+import {TableComponent} from '../../shared2223/components/table/table.component'
 @NgModule({
-  declarations: [Xvfc2223StateComponent, PtoComponent],
-  imports: [CommonModule, Xvfc2223StateRoutingModule],
+  declarations: [Xvfc2223StateComponent, PtoComponent, ReviewApplicationComponent],
+  imports: [CommonModule, Xvfc2223StateRoutingModule, TableComponent],
 })
 export class Xvfc2223StateModule {}
