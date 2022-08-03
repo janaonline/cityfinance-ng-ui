ef14c0605fc81fe4e140d66eeac5d2a647edb7c6
Ashu Gupta <ashu10832@gmail.com>
2022-08-03 16:14:59 +0530

    env update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -15,7 +15,7 @@ if (window.location.hostname.includes("new-cityfinance.dhwaniris.in")) {
   window.location.hostname.includes("localhost")
 ) {
   url = "https://democityfinanceapi.dhwaniris.in/api/v1/";
-  //  url = "https://newcityfinanceapi.dhwaniris.in/api/v1/";
+  // url = "http://localhost:8080/api/v1/";
   GoogleTagID = "UA-171288029-2";
 } else {
   url = "https://cityfinance.in/api/v1/";
