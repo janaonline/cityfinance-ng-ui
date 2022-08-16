9fecd389dec8784fb22b40a9001427c31f0f5e7e
chandra shekhar joshi <chandra.shekhar@dhwaniris.com>
2022-08-16 14:53:06 +0530

    state dashboard card data and form data dynamically adding data and ui structure

--- src/app/newPagesFc/xvfc2223-state/dashboard/dashboard.component.ts
+++ src/app/newPagesFc/xvfc2223-state/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { CompileShallowModuleMetadata } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,76 +8,169 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
   viewMode = 'tab1';
-  cardData : any = {
+  formdata: any;
+  navTabFirst: boolean = true;
+  navTabSecond: boolean = false;
+  navTabThird: boolean = false;
+  cardData: any = {
     title: 'card1',
     cardData: [{
-      icon:'../../../../assets/dashboard-state/16-location.svg',
-      link:'',
-      value:'4600',
-      lable:'Total ULBs',
-      color:''
+      icon: '../../../../assets/dashboard-state/16-location.svg',
+      link: '',
+      value: '4600',
+      lable: 'Total ULBs',
+      color: '#12505A;'
     },
     {
-      icon:'../../../../assets/dashboard-state/XMLID_1248_.svg',
-      link:'',
-      value:'500',
-      lable:'Non Million Cities',
-      color:''
+      icon: '../../../../assets/dashboard-state/XMLID_1248_.svg',
+      link: '',
+      value: '500',
+      lable: 'Non Million Cities',
+      color: '#12505A;'
     },
     {
-      icon:'../../../../assets/dashboard-state/sustainable.svg',
-      link:'',
-      value:'60',
-      lable:'Million Plus UAs',
-      color:''
+      icon: '../../../../assets/dashboard-state/sustainable.svg',
+      link: '',
+      value: '60',
+      lable: 'Million Plus UAs',
+      color: '#12505A;'
     },
     {
-      icon:'../../../../assets/dashboard-state/16-location.svg',
-      link:'',
-      value:'600',
-      lable:'ULBs in Million-Plus UAs',
-      color:''
+      icon: '../../../../assets/dashboard-state/16-location.svg',
+      link: '',
+      value: '600',
+      lable: 'ULBs in Million-Plus UAs',
+      color: '#12505A;'
     }]
   }
-  formData : any = [
+
+  formDataFirstInstallment: any = [
     {
-      formHeader:'ULB Forms',
-      formData :[{
-         formName: 'Annual Account Upload',
-         color:'#232334',
-         submittedValue:10,
-         approvedValue: 90,
-         icon:'',
-         link:''
+      formHeader: 'ULB Forms',
+      approvedColor: '#E67E15',
+      submittedColor: '#E67E1566',
+      formData: [{
+        formName: 'Annual Account Upload',
+        approvedColor: '#E67E15',
+        submittedColor: '#E67E1566',
+        submittedValue: 50,
+        approvedValue: 10,
+        icon: '',
+        link: '',
+        status: 'Not Started'
       },
       {
         formName: 'PFMS Linkage',
-        color:'#232323',
-        submittedValue:20,
-        approvedValue: 80,
-        icon:'',
-        link:''
+        approvedColor: '#E67E15',
+        submittedColor: '#E67E1566',
+        submittedValue: 40,
+        approvedValue: 30,
+        icon: '',
+        link: '',
+        status: 'Not Started'
       }]
     },
     {
-      formHeader:'State Forms',
-      formData :[{
+      formHeader: 'State Forms',
+      approvedColor: '#059B05',
+      submittedColor: '#E67E1566',
+      formData: [{
         formName: 'SFC Notification',
-        color:'#232334',
-        submittedValue:100,
-        approvedValue: 100
-     },
-     {
-       formName: 'Property Tax',
-       color:'#232323',
-       submittedValue:100,
-       approvedValue: 100
-     }]
+        approvedColor: '#059B05',
+        submittedColor: '#ffffff',
+        submittedValue: 100,
+        approvedValue: null,
+        icon: '',
+        link: '',
+        status: 'Approved'
+      },
+      {
+        formName: 'Property Tax',
+        approvedColor: '#059B05',
+        submittedColor: '#ffffff',
+        submittedValue: 100,
+        approvedValue: null,
+        icon: '',
+        link: '',
+        status: 'Approved'
+      }]
     }
-  ] 
-  constructor() { }
+  ]
+  formData2ndInstallment: any = [
+    {
+      formHeader: 'Sample ULB Forms',
+      approvedColor: '#E67E15',
+      submittedColor: '#E67E1566',
+      formData: [{
+        formName: 'Annual Account Upload',
+        approvedColor: '#E67E15',
+        submittedColor: '#E67E1566',
+        submittedValue: 50,
+        approvedValue: 30,
+        icon: '',
+        link: '',
+        status: 'Not Started'
+      },
+      {
+        formName: 'Sample PFMS Linkage',
+        approvedColor: '#E67E15',
+        submittedColor: '#E67E1566',
+        submittedValue: 40,
+        approvedValue: 20,
+        icon: '',
+        link: '',
+        status: 'Not Started'
+      }]
+    },
+    {
+      formHeader: 'Sample State Forms',
+      approvedColor: '#059B05',
+      submittedColor: '#E67E1566',
+      formData: [{
+        formName: 'SFC Notification',
+        approvedColor: '#059B05',
+        submittedColor: '#ffffff',
+        submittedValue: 100,
+        approvedValue: null,
+        icon: '',
+        link: '',
+        status: 'Approved'
+      },
+      {
+        formName: 'Sample Property Tax',
+        approvedColor: '#059B05',
+        submittedColor: '#ffffff',
+        submittedValue: 100,
+        approvedValue: null,
+        icon: '',
+        link: '',
+        status: 'Approved'
+      }]
+    }
+  ]
+  constructor() {
+    this.formdata = this.formDataFirstInstallment
+  }
 
   ngOnInit(): void {
   }
-
+  installmentClick(type) {
+    type == 'first' ? this.formdata = this.formDataFirstInstallment : this.formdata = this.formData2ndInstallment
+    console.log(type);
+  }
+  tabActive(tab) {
+    if (tab == 'tab1') {
+      this.navTabFirst = true
+      this.navTabSecond = false
+      this.navTabThird = false
+    } else if (tab == 'tab2') {
+      this.navTabSecond = true
+      this.navTabFirst = false
+      this.navTabThird = false
+    } else if (tab == 'tab3') {
+      this.navTabThird = true
+      this.navTabFirst = false
+      this.navTabSecond = false
+    }
+  }
 }
