aea01d4622af1e61d936afe223880f8b21a385b4
mazhar_fyR <mazhar_fyR@bitbucket.org>
2022-11-15 11:41:41 +0530

    fiscal ranking

---
+++ src/app/fiscal-ranking/fiscal-login/fiscal-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fiscal-login',
+  templateUrl: './fiscal-login.component.html',
+  styleUrls: ['./fiscal-login.component.scss']
+})
+export class FiscalLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/fiscal-ranking/fiscal-ranking.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FiscalRankingRoutingModule } from './fiscal-ranking-routing.module';
+import { FiscalHomeComponent } from './fiscal-home/fiscal-home.component';
+import { FiscalLoginComponent } from './fiscal-login/fiscal-login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
+
+
+
+@NgModule({
+  declarations: [FiscalHomeComponent, FiscalLoginComponent],
+  imports: [
+    CommonModule,
+    FiscalRankingRoutingModule,
+    MatFormFieldModule,
+    MatAutocompleteModule,
+    MatListModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatCardModule
+  ]
+})
+export class FiscalRankingModule { }
