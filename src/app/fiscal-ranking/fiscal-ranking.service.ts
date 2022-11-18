38ae3cc9696217e14df53810decf718abd4a817f
Pramod <impkagrahari@gmail.com>
2022-11-18 09:53:47 +0530

    ulb form fiscal ranking

---
+++ src/app/fiscal-ranking/fiscal-ranking.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { BehaviorSubject, Subject } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FiscalRankingService {
+
+  constructor(private http: HttpClient,) { }
+  getfiscalUlbForm() {
+    return this.http.get(
+      // `${environment.api.url}menu?role=ULB&year=606aafb14dff55e6c075d3ae&isUa=false`
+      `${environment.api.url}fiscal-ranking/view?design_year=606aadac4dff55e6c075c507&ulb=5dd24729437ba31f7eb42eb8`
+    );
+  }
+}
