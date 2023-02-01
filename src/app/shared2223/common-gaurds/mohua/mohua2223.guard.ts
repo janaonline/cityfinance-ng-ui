b84f00b04ad2d36da23ac0f8bda16f070890b1d1
NishantTheProgrammer <knishant113@gmail.com>
2023-02-01 11:00:39 +0530

    removed review-ulb-table component

--- src/app/shared2223/common-gaurds/mohua/mohua2223.guard.ts
+++ src/app/shared2223/common-gaurds/mohua/mohua2223.guard.ts
@@ -9,7 +9,6 @@ export class Mohua2223Guard implements CanActivate {
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
     let ulbRecord = JSON.parse(localStorage.getItem('userData'));
     let userType = ulbRecord?.role;
     if (userType == 'MoHUA' || userType == 'ADMIN' || userType == 'PARTNER') {
