f3e7cdd8f5c0b6358fc0a03290cf9a86a2ca2477
NishantTheProgrammer <knishant113@gmail.com>
2023-02-02 10:47:16 +0530

    review-ulb-table component code refectoring

--- src/app/fiscal-ranking/review-ulb-table/review-ulb-table.component.ts
+++ src/app/fiscal-ranking/review-ulb-table/review-ulb-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { State2223Service } from 'src/app/newPagesFc/xvfc2223-state/state-services/state2223.service';
 import { CommonService } from 'src/app/shared/services/common.service';
@@ -18,13 +19,47 @@ export class ReviewUlbTableComponent implements OnInit {
   userData;
   reviewTableName = 'Review Grant Application';
 
+  perPage: '10' | '25' | '50' | '100' | 'all' = '10';
+  filterForm: FormGroup;
+  isLoader: boolean = false;
+  max = Math.max;
+
+
+
+  tableDefaultOptions = {
+    itemPerPage: 10,
+    currentPage: 1,
+    totalCount: null,
+  };
+
+
+  listFetchOption = {
+    filter: null,
+    sort: null,
+    csv: false,
+    skip: 0,
+    limit: this.tableDefaultOptions.itemPerPage,
+  };
+
   constructor(
     private commonService: NewCommonService,
     private route: ActivatedRoute,
+    private _fb: FormBuilder,
     private stateServices: State2223Service,
     private _commonService: CommonService) {
   }
   ngOnInit(): void {
+    this.filterForm = this._fb.group({
+      ulb_name_s: [""],
+      state_name_s: [""],
+      ulb_code_s: [""],
+      ulbType_s: [""],
+      population_type_s: [""],
+      ua_name_s: [""],
+      status_s: [""],
+      filled_1: [""],
+      filled_2: [""],
+    });
     this.fetchStateList();
     this.userData = JSON.parse(localStorage.getItem("userData"));
     this.loadData();
@@ -33,6 +68,16 @@ export class ReviewUlbTableComponent implements OnInit {
   get year() {
     const yearItems = JSON.parse(localStorage.getItem('Years'))
     return yearItems['2022-23'];
+  }
+
+
+  get isInfiniteScroll() {
+    return this.perPage == 'all';
+  }
+
+  get showableColumns() {
+    const hiddenStateNames = ['stateName', 'filled_audited', 'filled_provisional'];
+    return this.objectWithoutProperties(this.columnNames, hiddenStateNames);
   }
 
   loadData() {
@@ -51,22 +96,48 @@ export class ReviewUlbTableComponent implements OnInit {
   }
 
 
-  filterAdded(event) {
-    console.log("drop down changes state", event);
-    this.stateId = event;
-    this.loadData();
-    // this.stateServices.dpReviewChanges.next(true);
-  }
-
-  filterUpdate(event) {
-    console.log(event);
-  }
-
-  
-  private fetchStateList() {
+  fetchStateList() {
     this._commonService.getStateUlbCovered().subscribe((res) => {
       this.stateList = res.data;
     });
+  }
+
+
+
+  objectWithoutProperties(obj, keys) {
+    const target = {};
+    for (let i in obj) {
+      if (keys.indexOf(i) >= 0) continue;
+      if (!Object.prototype.hasOwnProperty.call(obj, i)) continue;
+      target[i] = obj[i];
+    }
+    return target;
+  }
+
+
+
+  keepOriginalOrder = (a, b) => a.key;
+
+  onPerPageChange() {
+
+  }
+
+  download() {
+
+  }
+
+  handleScroll(event) {
+
+  }
+
+  search() {
+    const payload = { ...this.filterForm.getRawValue(), ...this.listFetchOption };
+    console.log(payload);
+  }
+
+  resetFilter() {
+    this.filterForm.reset();
+    this.search();
   }
 }
 
