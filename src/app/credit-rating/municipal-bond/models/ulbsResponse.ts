0cf37c55b3321d7d80358e0aa9f7c43dd27fe6c0
NishantTheProgrammer <knishant113@gmail.com>
2023-02-14 18:24:27 +0530

    added types Rows, Columns, MouProjectsResponse

--- src/app/credit-rating/municipal-bond/models/ulbsResponse.ts
+++ src/app/credit-rating/municipal-bond/models/ulbsResponse.ts
@@ -11,3 +11,38 @@ export interface ULB {
   state?: string; // State Id
   stateName: string;
 }
+
+
+export interface Rows {
+  projectName: string;
+  implementationAgency: string;
+  totalProjectCost: number;
+  stateShare: number;
+  ulbShare: number;
+  capitalExpenditureState: number;
+  capitalExpenditureUlb: number;
+  omExpensesState: number;
+  omExpensesUlb: number;
+  sector: string;
+  startDate: string;
+  estimatedCompletionDate: string;
+  moreInformation: Link;
+  projectReport: Link;
+  creditRating: Link;
+}
+export interface Link {
+  name: string;
+  url: string;
+}
+export interface Columns {
+  label: string;
+  key: string;
+  databaseKey?: string | boolean;
+}
+export interface MouProjectsResponse {
+  success: boolean;
+  message: string;
+  rows?: (Rows)[] | null;
+  filters: any;
+  columns?: (Columns)[] | null;
+}
