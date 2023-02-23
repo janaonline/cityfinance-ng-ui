c238cd8229931567e7aeecdacab33994ce2bb34e
NishantTheProgrammer <knishant113@gmail.com>
2023-02-23 18:39:04 +0530

    added municipal-bond-repository and routing setup

---
+++ src/app/pages/resources-dashboard/learning-center/municipal-bond-repository/municipal-bond-repository.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-municipal-bond-repository',
+  templateUrl: './municipal-bond-repository.component.html',
+  styleUrls: ['./municipal-bond-repository.component.scss']
+})
+export class MunicipalBondRepositoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
