ee034c85fa5f87e6b6df14b130e9085df4e1ad2f
NishantTheProgrammer <knishant113@gmail.com>
2023-03-17 17:45:17 +0530

    prestine and UI issue fixed

--- src/app/fiscal-ranking/ulb-fiscal-new/ulb-fiscal-new.component.ts
+++ src/app/fiscal-ranking/ulb-fiscal-new/ulb-fiscal-new.component.ts
@@ -425,6 +425,7 @@ export class UlbFiscalNewComponent implements OnInit {
           },
         }).then((value) => {
           if (value == 'draft') this.submit();
+          else this.fiscalForm.markAsPristine();
         });
       }
     });
