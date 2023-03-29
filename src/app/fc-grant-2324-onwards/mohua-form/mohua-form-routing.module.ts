603ca3b3a27afedd65ca2c6b811fd1c8a600a734
Pramod <impkagrahari@gmail.com>
2023-03-29 13:32:22 +0530

    mohua routes added

--- src/app/fc-grant-2324-onwards/mohua-form/mohua-form-routing.module.ts
+++ src/app/fc-grant-2324-onwards/mohua-form/mohua-form-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Mohua2223Guard } from 'src/app/shared2223/common-gaurds/mohua/mohua2223.guard';
+import { MohuaFormComponent } from './mohua-form.component';
+import { ReviewUlbTableComponent } from './review-ulb-table/review-ulb-table.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: MohuaFormComponent,
+    canActivate: [Mohua2223Guard],
+    children: [
+      // {
+      //   path: "dashboard",
+      //   component: DashbordComponent,
+      // },
+      {
+        path: "review-ulb-form",
+        component: ReviewUlbTableComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
