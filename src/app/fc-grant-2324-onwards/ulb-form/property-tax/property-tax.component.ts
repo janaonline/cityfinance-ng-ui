a96024d2c0a3300066a3a692d3dca93ef008d58a
NishantTheProgrammer <knishant113@gmail.com>
2023-03-31 12:45:16 +0530

    added property-tax service

--- src/app/fc-grant-2324-onwards/ulb-form/property-tax/property-tax.component.ts
+++ src/app/fc-grant-2324-onwards/ulb-form/property-tax/property-tax.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PropertyTaxService } from './property-tax.service';
 
 @Component({
   selector: 'app-property-tax',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyTaxComponent implements OnInit {
 
-  constructor() { }
+  userData = JSON.parse(localStorage.getItem("userData"));
+
+  constructor(
+    private propertyTaxService: PropertyTaxService
+  ) { }
 
   ngOnInit(): void {
+
+    this.loadData();
   }
+
+  get design_year() {
+    const years = JSON.parse(localStorage.getItem("Years"));
+    return years?.['2023-24'];
+  }
+
+  get ulbId() {
+    return this.userData?.ulb;
+  }
+
+
+  loadData() {
+    this.propertyTaxService.getForm(this.ulbId, this.design_year, '6').subscribe(res => {
+      console.log('response', res);
+    });
+  }
+
 
 }
