c07eaa73b48e40c37a16c635d438b86f4feb6cc1
Pramod_Agrahari <pramod.kuamr@dhwaniris.com>
2023-05-29 16:27:32 +0530

    INdicator wss

--- src/app/fc-grant-2324-onwards/state-form/indicators-wss/indicators-wss.component.ts
+++ src/app/fc-grant-2324-onwards/state-form/indicators-wss/indicators-wss.component.ts
@@ -8,7 +8,15 @@ import { Component, OnInit } from '@angular/core';
 export class IndicatorsWssComponent implements OnInit {
 
   constructor() { }
-
+  isApiInProgress:boolean = false;
+  response = {
+    formName: 'Indicators for Water Supply and Sanitation',
+    formId: '',
+    status: '',
+    statusId: '',
+    info: 'The below tables denotes the aggregate indicators and targets of ULBs in respective UA',
+    previousYrMsg: '',
+  }
   ngOnInit(): void {
     
   }
