a2e260de3ab1b563f8528b8bab7eb51d5da613b0
Pramod_Agrahari <pramod.kuamr@dhwaniris.com>
2023-06-28 18:54:40 +0530

    submodule update

--- src/app/fc-grant-2324-onwards/fc-shared/components/state-common-review/state-common-review.component.ts
+++ src/app/fc-grant-2324-onwards/fc-shared/components/state-common-review/state-common-review.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-state-common-review',
@@ -8,8 +8,32 @@ import { Component, OnInit } from '@angular/core';
 export class StateCommonReviewComponent implements OnInit {
 
   constructor() { }
+  
+  @Input() canTakeAction:boolean = false;
+  @Input() uploadFolderName:string = '';
+  @Input() errorInAction = false;
+  @Input() isActionSubmitted = false;
+  @Input() actBtnDis:boolean = false;
+  @Input() formData;
+  @Output() formChangeEventEmit = new EventEmitter<string>();
+  @Input() question;
+
+  statusIdForApprove:string = '6';
+  statusIdForReject:string = '7';
+  Years = JSON.parse(localStorage.getItem("Years"));
+  userData = JSON.parse(localStorage.getItem("userData"));
 
   ngOnInit(): void {
+    console.log('formdata', this.formData);
+    
   }
 
+  formValueChange(event, type, question){
+    console.log('review', event, type, question)
+    let obj =  this.formData.uaData.filter((el)=>{
+      el?.ua == question?.ua
+    })
+
+    console.log('review obj obj', obj)
+  }
 }
