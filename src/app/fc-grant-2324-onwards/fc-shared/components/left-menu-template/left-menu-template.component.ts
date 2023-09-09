69b5fe80c2ed8b020be7c7d97a46b888e3d055c5
NishantTheProgrammer <knishant113@gmail.com>
2023-09-09 11:53:53 +0530

    Remove left menu header for PMU

--- src/app/fc-grant-2324-onwards/fc-shared/components/left-menu-template/left-menu-template.component.ts
+++ src/app/fc-grant-2324-onwards/fc-shared/components/left-menu-template/left-menu-template.component.ts
@@ -1,5 +1,6 @@
 import { KeyValue } from '@angular/common';
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { UserUtility } from 'src/app/util/user/user';
 
 @Component({
   selector: 'app-left-menu-template',
@@ -7,24 +8,26 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
   styleUrls: ['./left-menu-template.component.scss']
 })
 export class LeftMenuTemplateComponent implements OnInit {
-
-  constructor() { }
-@Input() leftMenu = {};
+  role: string;
+  constructor() {
+    this.role = new UserUtility().getLoggedInUserDetails().role;
+  }
+  @Input() leftMenu = {};
 
   ngOnInit(): void {
 
   }
   // ngOnChanges(changes: SimpleChanges){
-   // if(changes?.leftMenu && changes?.leftMenu?.currentValue;)
+  // if(changes?.leftMenu && changes?.leftMenu?.currentValue;)
   //   this.leftMenu = changes.leftMenu.currentValue;
   // }
 
-  returnPostion = (a: KeyValue<number,string>, b: KeyValue<number,string>): number => {
-    let val_1 : any = a;
-    let val_2 : any = b;
+  returnPostion = (a: KeyValue<number, string>, b: KeyValue<number, string>): number => {
+    let val_1: any = a;
+    let val_2: any = b;
     val_1 = (val_1.key?.split('_'))[1];
     val_2 = (val_2.key?.split('_'))[1];
-   return val_1 > val_2 ? 1 : (val_2 > val_1 ? -1 : 0);
+    return val_1 > val_2 ? 1 : (val_2 > val_1 ? -1 : 0);
   }
 
 }
