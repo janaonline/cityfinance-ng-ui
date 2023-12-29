a7d37f846621cab55a1ec83102d784f390ce78c0
NishantTheProgrammer <knishant113@gmail.com>
2023-12-29 17:28:29 +0530

    separating numbers with commas

--- src/app/fiscal-ranking/fiscal-ranking.module.ts
+++ src/app/fiscal-ranking/fiscal-ranking.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from '@angular/core';
+import "@angular/common/locales/global/en-IN";
+
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatStepperModule } from '@angular/material/stepper';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FiscalRankingRoutingModule } from './fiscal-ranking-routing.module';
 import { FiscalHomeComponent } from './fiscal-home/fiscal-home.component';
 import { FiscalLoginComponent } from './fiscal-login/fiscal-login.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from "@angular/material/autocomplete";
 import { MatListModule } from "@angular/material/list";
 import { MatMenuModule } from "@angular/material/menu";
@@ -21,7 +23,7 @@ import { MatCardModule } from "@angular/material/card";
 import { SharedModule } from '../shared/shared.module';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { LoaderComponent } from './loader/loader.component';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatRadioModule } from '@angular/material/radio';
 import { UlbFisPreviewComponent } from './ulb-fiscal-new/ulb-fis-preview/ulb-fis-preview.component';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RecaptchaModule } from 'ng-recaptcha';
@@ -72,12 +74,13 @@ import { ParticipatingUlbsComponent } from './participating-ulbs/participating-u
 import { IndiaMapComponent } from './india-map/india-map.component';
 import { FileUrlCheckPipe } from './pipes/file-url-check.pipe';
 import { VideosPopupComponent } from './home/videos-popup/videos-popup.component';
+import { TypeofPipe } from './pipes/typeof.pipe';
 
 @NgModule({
   declarations: [
     FiscalHomeComponent,
     FiscalLoginComponent,
-   // UlbFiscalComponent,
+    // UlbFiscalComponent,
     UlbFiscalNewComponent,
     LoaderComponent,
     UlbFisPreviewComponent,
@@ -122,7 +125,8 @@ import { VideosPopupComponent } from './home/videos-popup/videos-popup.component
     ParticipatingUlbsComponent,
     IndiaMapComponent,
     FileUrlCheckPipe,
-    VideosPopupComponent
+    VideosPopupComponent,
+    TypeofPipe
   ],
   imports: [
     CommonModule,
@@ -158,9 +162,12 @@ import { VideosPopupComponent } from './home/videos-popup/videos-popup.component
     AngularMultiSelectModule,
     MatProgressSpinnerModule
   ],
-  providers: [{
-    provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false }
-  }],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS, useValue: { displayDefaultIndicatorType: false },
+    },
+    { provide: LOCALE_ID, useValue: "en-IN" }
+  ],
   exports: [LoaderComponent]
   // providers: [
   //   {
