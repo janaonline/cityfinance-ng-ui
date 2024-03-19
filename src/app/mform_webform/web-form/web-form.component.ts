4ca31a1a607e0fd33722c658ae4b6b452ed4600e
Pramod Agrahari <pramod.kumar@dhwaniris.com>
2024-03-19 15:33:46 +0530

    dur pagination

--- src/app/mform_webform/web-form/web-form.component.ts
+++ src/app/mform_webform/web-form/web-form.component.ts
@@ -8,7 +8,8 @@ import {
   OnDestroy,
   OnChanges,
   SimpleChanges,
-  TemplateRef
+  TemplateRef,
+  ViewChild
 } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { List, update } from 'immutable';
@@ -45,6 +46,7 @@ import { getDaysDifference, isValidDate } from './utilities/general';
 import { SelectDeletableComponent } from './select-deletable/select-deletable.component';
 import { CommonServicesService } from 'src/app/fc-grant-2324-onwards/fc-shared/service/common-services.service';
 import { ActivatedRoute } from '@angular/router';
+import { MatPaginator } from '@angular/material/paginator';
 const swal: SweetAlert = require("sweetalert");
 
 declare const $: any;
@@ -147,7 +149,8 @@ export class WebFormComponent implements OnInit, OnDestroy, OnChanges {
   showForm: boolean = true;
   selectedYearId:string="";
   selectedYear:string="";
-
+  @ViewChild('paginator') paginator: MatPaginator;
+  
   ngOnInit() {
     this.getQueryParams();
     if (
@@ -2680,6 +2683,9 @@ export class WebFormComponent implements OnInit, OnDestroy, OnChanges {
   pageChange(question, { pageIndex, pageSize }) {
     this.pageSize = pageSize;
     question.scrollIndex = pageIndex * pageSize;
+    if(pageIndex === 0){
+      this.paginator.firstPage();
+    }
   }
   
   getQueryParams() {
