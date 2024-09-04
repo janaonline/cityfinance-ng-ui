5fef83c5abdd90362aab001659b2d801c5361469
Navinder Singh <146955910+navinder-dev@users.noreply.github.com>
2024-09-04 19:39:48 +0530

    2425 GTC download - redirect to "review" API.

--- src/app/shared2223/components/table/table.component.ts
+++ src/app/shared2223/components/table/table.component.ts
@@ -475,8 +475,8 @@ export class TableComponent implements OnInit, OnChanges, OnDestroy {
     // const endPoint = this.designYear == this.years["2023-24"] ? this.endPoint : "review" ;
     const year = this._commonService2324.getYearName(this.designYear);
     const yearSplit = Number(year.split('-')[0]);
-    const endPoint = yearSplit >= 2023 ? this.endPoint : "review";
-    // console.log(params);
+    let endPoint = yearSplit >= 2023 ? this.endPoint : "review";
+    if (params.formId == '7') endPoint = "review";
     this._commonService.openWindowToDownloadCsv(params, endPoint);
   }
   
