9f80b869e4fdbda63ed43a8c63cb9c3c6893fcff
mdshamoon <mohdshamoon841@gmail.com>
2024-10-29 00:53:52 +0530

    updated deploy

--- scripts/before_intall.sh
+++ scripts/before_intall.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 # Before install
+
+if [ -d /var/www/html/cityfinance/cityfinancev1 ]; then
+  echo "Deleting /var/www/html/cityfinance/cityfinancev1..."
+  rm -rf /var/www/html/cityfinance/cityfinancev1
+fi
