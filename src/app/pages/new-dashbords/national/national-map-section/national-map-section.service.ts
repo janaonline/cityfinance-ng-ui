6c3a3e1ed06fa2a1a7586f20476c0ff86d2ef833
Sashikant-jg <sashikant.agarwal@janaagraha.org>
2025-01-13 14:25:55 +0530

    new api added for data availability for national dashbaord

--- src/app/pages/new-dashbords/national/national-map-section/national-map-section.service.ts
+++ src/app/pages/new-dashbords/national/national-map-section/national-map-section.service.ts
@@ -39,7 +39,7 @@ export class NationalMapSectionService {
   getNationalData(nationalInput) {
     return this.http.get(
       environment.api.url +
-        `national-dashboard/data-availability?financialYear=${nationalInput.financialYear}&stateId=${nationalInput.stateId}&population=${nationalInput.populationCat}&ulbType=${nationalInput.ulbType}`
+        `national-performance-dashboard/data-availability?financialYear=${nationalInput.financialYear}&stateId=${nationalInput.stateId}&population=${nationalInput.populationCat}&ulbType=${nationalInput.ulbType}`
     );
   }
 
