f6c500077691b135e8ce45bc9d0e8a6103a3bc4c
Navinder Singh <navinder.singh@janaagraha.org>
2025-02-18 13:04:21 +0530

    Make DUR pdf downlod dynamic.

---
+++ src/app/newPagesFc/xvfc2223-ulb/detailed-utilization-report/dur-dump/dur-dump.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ulbs_data } from "src/app/newPagesFc/xvfc2223-ulb/detailed-utilization-report/ulbsData";
+
+@Component({
+  selector: 'app-dur-dump',
+  templateUrl: './dur-dump.component.html',
+  styleUrls: ['./dur-dump.component.scss']
+})
+export class DurDumpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    //<-----------------------------------bulk pdf download----------------------------->
+    // for 2022-23
+    // ulbs_data.slice(0, 20).forEach(ulb => {
+    //   window.open(`/ulbform2223/utilisation-report/${ulb.ulbId}?ulbName=${ulb.ulbName}&ulbCode=${ulb.ulbCode}&stateName=${ulb.stateName}&status=${ulb?.formStatus}`, '_blank')
+    // });
+
+    // for 2023-24
+    ulbs_data.slice(60, 70).forEach(ulb => {
+      console.log("-------------------data--------------->", ulb.ulbId, ulb.ulbName, ulb.ulbCode, ulb.stateName, ulb.formStatus)
+      window.open(`/ulb-form/606aafc14dff55e6c075d3ec/utilisation-report/${ulb.ulbId}?ulbName=${ulb.ulbName}&ulbCode=${ulb.ulbCode}&stateName=${ulb.stateName}&status=${ulb?.formStatus}`, '_blank')
+    });
+  }
+
+}
