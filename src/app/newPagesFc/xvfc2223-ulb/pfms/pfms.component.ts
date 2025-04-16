ee1c53ec4b8a0f92bcd5c2eb2a45f245437af5e8
Sashikant-jg <sashikant.agarwal@janaagraha.org>
2025-04-16 10:42:43 +0530

    pfms-22-23 issues fixed

--- src/app/newPagesFc/xvfc2223-ulb/pfms/pfms.component.ts
+++ src/app/newPagesFc/xvfc2223-ulb/pfms/pfms.component.ts
@@ -111,6 +111,7 @@ export class PfmsComponent implements OnInit, OnDestroy {
   previewData: any;
   next_router = "#";
   isDisabled: boolean = false;
+  isLinkedPMFSDisabled:boolean=true
   dataValue: any;
   uploadedFile: any;
   disableInputs: boolean = false;
@@ -245,6 +246,13 @@ export class PfmsComponent implements OnInit, OnDestroy {
       },
       (error) => {
         this.isDisabled = true;
+        this.registerForm = this.formBuilder.group({
+          linkPFMS: 'Yes',
+          isUlbLinkedWithPFMS: 'Yes',
+          PFMSAccountNumber: '',
+          ulb: this.ulbId,
+          design_year: this.designYearId,
+        });
         this.isApiInProgress = false;
         if (this.ulbData?.role !== "ULB") {
           this.isDisabled = false;
@@ -284,8 +292,8 @@ export class PfmsComponent implements OnInit, OnDestroy {
   patchValues() {
     console.log("this.dataValue", this.dataValue);
     this.registerForm.patchValue({
-      linkPFMS: this.dataValue?.data?.linkPFMS,
-      isUlbLinkedWithPFMS: this.dataValue?.data?.isUlbLinkedWithPFMS,
+      linkPFMS: this.dataValue?.data?.linkPFMS ? this.dataValue?.data?.linkPFMS : 'Yes',
+      isUlbLinkedWithPFMS: this.dataValue?.data?.isUlbLinkedWithPFMS ?  this.dataValue?.data?.isUlbLinkedWithPFMS:'Yes',
       PFMSAccountNumber: this.dataValue?.data?.PFMSAccountNumber,
       ulb: this.ulbId,
       design_year: this.designYearId,
