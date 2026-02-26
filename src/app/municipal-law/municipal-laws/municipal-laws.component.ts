857ab2a3deea5ba6836b574c07cb5568e1215dba
Kushal-MP08 <kushal.mp@janaagrha.org>
2026-02-26 17:48:57 +0530

    fixed static data path

--- src/app/municipal-law/municipal-laws/municipal-laws.component.ts
+++ src/app/municipal-law/municipal-laws/municipal-laws.component.ts
@@ -132,7 +132,7 @@ export class MunicipalLawsComponent implements OnInit {
   }
   fetchMunicipalLawJSONFile() {
     this.http
-      .get("/assets/files/municipal-laws.json")
+      .get("./assets/files/municipal-laws.json")
       .subscribe((data: any[]) => {
         this.list = data;
       });
