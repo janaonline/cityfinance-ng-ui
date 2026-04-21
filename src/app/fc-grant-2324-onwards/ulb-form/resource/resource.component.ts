d7eebf8e8789348f2ce4018f6251b97398c61593
Navinder Singh <navinder.singh@janaagraha.org>
2026-04-21 16:08:51 +0530

    feat(ulb-resources): #DJ2-127 | update resource component with new data and links; add ULBs Financial Diagnosis files

--- src/app/fc-grant-2324-onwards/ulb-form/resource/resource.component.ts
+++ src/app/fc-grant-2324-onwards/ulb-form/resource/resource.component.ts
@@ -13,43 +13,48 @@ export class ResourceComponent implements OnInit {
   constructor() {
     this.sideMenuItem = JSON.parse(localStorage.getItem("leftMenuULB"));
   }
-
   resoucesData = [
     {
+      imgurl: "./assets/ulbform/overview/ULBs-Financial-Diagosis.jpg",
+      link: "./assets/files/ULBs%20Financial%20Diagosis.xlsx",
+      fileName: `ULBs Financial Diagosis`,
+      icon: "",
+    },
+    {
       imgurl: "./assets/ulbform/overview/Picture3.png",
-      link: "https://staging.cityfinance.in/assets/files/ULB%20Nodal%20Officers%20Manual%20for%20Claiming%20XV%20FC%20ULB%20Grants%20Oct%202021.pdf",
+      link: "./assets/files/ULB%20Nodal%20Officers%20Manual%20for%20Claiming%20XV%20FC%20ULB%20Grants%20Oct%202021.pdf",
       fileName: `ULB Nodal Officers Manual for Claiming XV FC ULB Grants for 2021-22`,
       icon: "",
     },
     {
       imgurl: "./assets/ulbform/overview/Picture3.png",
-      link: "https://staging.cityfinance.in/assets/files/State%20Nodal%20Officers%20Manual%20for%20Claiming%20XV%20FC%20ULB%20Grants%20Oct%202021.pdf",
+      link: "./assets/files/State%20Nodal%20Officers%20Manual%20for%20Claiming%20XV%20FC%20ULB%20Grants%20Oct%202021.pdf",
       fileName:
         "State Nodal Officers Manual for Claiming XV FC ULB Grants for 2021-22",
       icon: "",
     },
     {
       imgurl: "./assets/ulbform/overview/Picture1.png",
-      link: "https://staging.cityfinance.in/assets/files/XVFC%20VOL%20I%20Main%20Report%202021-26.pdf",
+      link: "./assets/files/XVFC%20VOL%20I%20Main%20Report%202021-26.pdf",
       fileName: "XV-FC VOL I Main Report 2021-26",
       icon: "",
     },
     {
       imgurl: "./assets/ulbform/overview/Picture1.png",
-      link: "https://staging.cityfinance.in/assets/files/XV-FC%20-VOL%20II%20Annexes.pdf",
+      link: "./assets/files/XV-FC%20-VOL%20II%20Annexes.pdf",
       fileName: "XV-FC -VOL II Annexes 2021-26",
       icon: "",
     },
     {
       imgurl: "./assets/ulbform/overview/Picture2.png",
-      link: "https://staging.cityfinance.in/assets/files/FC-XV%20recommended%20Urban%20Local%20Body%20Final%20Operational%20Guidelines%20for%202021-26.pdf",
+      link: "./assets/files/FC-XV%20recommended%20Urban%20Local%20Body%20Final%20Operational%20Guidelines%20for%202021-26.pdf",
       fileName:
         "XV-FC recommended Urban Local Body Final Operational Guidelines for 2021-26XV-FC recommended Urban Local Body Final Operational Guidelines for 2021-26",
       icon: "",
     },
     {
       imgurl: "./assets/ulbform/overview/Picture4.png",
-      link: "https://staging.cityfinance.in/assets/files/13thFC-ManualGOIULB.pdf",
+      link: "./assets/files/13thFC-ManualGOIULB.pdf",
       fileName: "National Municipal Accounting Manual",
       icon: "",
     },
